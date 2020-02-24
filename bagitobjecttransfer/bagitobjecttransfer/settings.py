191f71a02b201a3f2feb1dfd07ae522cea7a6d7a
Daniel Lovegrove <d.lovegrove11@gmail.com>
2020-02-24 11:32:36 -0600

    Add console INFO logging settings

--- bagitobjecttransfer/bagitobjecttransfer/settings.py
+++ bagitobjecttransfer/bagitobjecttransfer/settings.py
@@ -99,6 +99,23 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO')
+        }
+    }
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
