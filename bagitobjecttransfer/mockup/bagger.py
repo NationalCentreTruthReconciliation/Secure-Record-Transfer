21ba2fe7ffefdf719342a87551554b8e001a147e
Daniel Lovegrove <d.lovegrove11@gmail.com>
2020-02-24 11:35:30 -0600

    Start building a bagging class

---
+++ bagitobjecttransfer/mockup/bagger.py
@@ -0,0 +1,14 @@
+import bagit
+
+class Bagger:
+
+    def create_bag(self, files, metadata):
+        # Create a folder to bag
+        # Copy the files to the new folder
+        # Bag the folder with bagit, pass the metadata to it
+        # Verify the bag
+
+        # Return information dictionary
+        return {
+            'success': True
+        }
