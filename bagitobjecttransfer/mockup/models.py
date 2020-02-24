6b4f94dd27ff5e9d6572f3f0e0a28c40e8362cb0
Daniel Lovegrove <d.lovegrove11@gmail.com>
2020-02-24 15:08:20 -0600

    Add a couple of models and migrations

--- bagitobjecttransfer/mockup/models.py
+++ bagitobjecttransfer/mockup/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class AppUser(models.Model):
+    name = models.CharField(max_length=256)
+    email = models.CharField(max_length=256)
+
+    def __str__(self):
+        return f'{self.name}, {self.email}'
+
+class Bag(models.Model):
+    bagging_date = models.DateTimeField()
+    bag_location = models.CharField(max_length=256)
+    user = models.ForeignKey(AppUser, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'"{self.bag_location}" bagged at {self.bagging_date} by {self.user.name}'
