988fa75fa85a5461893146fedf7f2e83c172051a
Daniel Lovegrove <d.lovegrove11@gmail.com>
2020-02-25 10:22:05 -0600

    Handle form submission with redirect

--- bagitobjecttransfer/mockup/urls.py
+++ bagitobjecttransfer/mockup/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'mockup'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('transfer/', views.transfer, name='transfer')
+    path('transfer/', views.transfer, name='transfer'),
+    path('transfer/send/', views.sendtransfer, name='sendtransfer'),
+    path('transfer/sent/', views.transfersent, name='transfersent'),
 ]
--- bagitobjecttransfer/mockup/views.py
+++ bagitobjecttransfer/mockup/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 def index(request):
     return render(request, 'mockup/home.html')
 
 def transfer(request):
     return render(request, 'mockup/transfer.html')
+
+def sendtransfer(request):
+    if request.method == 'POST':
+        try:
+            first_name = request.POST['first_name']
+            last_name = request.POST['last_name']
+            print (f'First Name: {first_name} | Last Name: {last_name}')
+        except KeyError:
+            return render(request, 'mockup/transfer.html', {
+                'error_msg': 'Transfer Failed, could not find first or last name in POST.'
+            })
+        else:
+            return HttpResponseRedirect(reverse('mockup:transfersent'))
+    else:
+        return render(request, 'mockup/transfer.html')
+
+def transfersent(request):
+    return render(request, 'mockup/transfersent.html')
