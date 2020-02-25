4da2bdc51ed582c52e689ed2b230584a147c49f1
Daniel Lovegrove <d.lovegrove11@gmail.com>
2020-02-25 09:23:40 -0600

    Add app_name to urls

--- bagitobjecttransfer/mockup/urls.py
+++ bagitobjecttransfer/mockup/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'mockup'
 urlpatterns = [
     path('', views.index, name='index'),
     path('transfer/', views.transfer, name='transfer')
