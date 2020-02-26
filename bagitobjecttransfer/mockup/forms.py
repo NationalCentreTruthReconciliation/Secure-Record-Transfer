d5d997cc1e4528e393d1543862f1e6b65766a94f
Daniel Lovegrove <d.lovegrove11@gmail.com>
2020-02-26 16:30:37 -0600

    Finish adding field placeholders

--- bagitobjecttransfer/mockup/forms.py
+++ bagitobjecttransfer/mockup/forms.py
@@ -3,13 +3,13 @@ from django import forms
 
 class TransferForm(forms.Form):
     first_name = forms.CharField(
-        max_length=64,
+        max_length=32,
         min_length=2,
         widget=forms.TextInput(attrs={'placeholder': 'Enter your first name'}),
     )
 
     last_name = forms.CharField(
-        max_length=64,
+        max_length=32,
         min_length=2,
         widget=forms.TextInput(attrs={'placeholder': 'Enter your last name'}),
     )
@@ -17,27 +17,32 @@ class TransferForm(forms.Form):
     phone_number = forms.RegexField(
         regex=r'^\d{10}$',
         error_messages={
-            'invalid': 'Phone number must be in the form "9999999999"'
-        }
+            'invalid': 'Phone number must contain ten digits without spaces'
+        },
+        widget=forms.TextInput(attrs={'placeholder': 'Enter your phone number'}),
     )
 
-    email = forms.EmailField()
+    email = forms.EmailField(
+        widget=forms.TextInput(attrs={'placeholder': 'Enter your email'}),
+    )
 
     organization = forms.CharField(
         max_length=128,
         min_length=2,
         required=False,
+        widget=forms.TextInput(attrs={'placeholder': 'Enter your organization (optional)'}),
     )
 
     organization_address = forms.CharField(
         max_length=256,
         min_length=8,
         required=False,
+        widget=forms.TextInput(attrs={'placeholder': 'Enter your organization\'s address (optional)'}),
     )
 
     description = forms.CharField(
         widget=forms.Textarea(attrs={
             'rows': '4',
-            'placeholder': 'Enter a brief description of your uploaded files'
+            'placeholder': 'Enter a brief description of your uploaded files',
         }),
     )
