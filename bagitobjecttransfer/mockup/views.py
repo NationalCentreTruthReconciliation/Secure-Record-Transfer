b656da6c9a6ab070eb43afb8de26ef026d1f44a2
Daniel Lovegrove <d.lovegrove11@gmail.com>
2020-02-28 15:08:36 -0600

    Create bag from POSTed files

--- bagitobjecttransfer/mockup/views.py
+++ bagitobjecttransfer/mockup/views.py
@@ -4,6 +4,10 @@ from django.urls import reverse
 from django.views.generic import TemplateView, FormView
 
 from .forms import TransferForm
+from .appsettings import bag_storage_folder
+
+from .bagger import Bagger
+
 
 class Index(TemplateView):
     template_name = 'mockup/home.html'
@@ -21,11 +25,21 @@ class TransferSent(TemplateView):
 def sendtransfer(request):
     if request.method == 'POST':
         try:
-            form = TransferForm(request.POST)
+            form = TransferForm(request.POST, request.FILES)
             if form.is_valid():
                 first_name = form.cleaned_data['first_name']
                 last_name = form.cleaned_data['last_name']
                 print (f'First Name: {first_name} | Last Name: {last_name}')
+                files = request.FILES.getlist('upload_files')
+                file_list = []
+                for f in files:
+                    file_list.append({
+                        'filepath': f.temporary_file_path(),
+                        'name': f.name
+                    })
+
+                Bagger.create_bag(bag_storage_folder, file_list, {})
+
             else:
                 print ('Form was invalid...')
                 return render(request, 'mockup/transfer.html', {'form': form})
