63a62abc847143681b537fbead0dcedc4a56e2e2
Daniel Lovegrove <d.lovegrove11@gmail.com>
2020-02-28 15:06:44 -0600

    Add form field to TransferForm

--- bagitobjecttransfer/mockup/forms.py
+++ bagitobjecttransfer/mockup/forms.py
@@ -46,3 +46,9 @@ class TransferForm(forms.Form):
             'placeholder': 'Enter a brief description of your uploaded files',
         }),
     )
+
+    upload_files = forms.FileField(
+        widget=forms.ClearableFileInput(attrs={
+            'multiple': True
+        })
+    )
