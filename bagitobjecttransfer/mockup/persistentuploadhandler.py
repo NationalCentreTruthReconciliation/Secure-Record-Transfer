7b17acca6a10adb78713c4db662bdf1bdb961218
Daniel Lovegrove <d.lovegrove11@gmail.com>
2020-03-11 08:49:29 -0500

    Create PersistentUploadedFileHandler with manual file removal
    There are two new classes in this file.
    1. PersistentUploadedFile, which is a copy of TemporaryUploadedFile with
    one small change: the delete flag on the NamedTemporaryFile is set to
    False, so that the temporary file must be removed manually.
    
    2. PersistentFileUploadHandler, which is a copy of
    TemporaryFileUploadHandler, but it uses the new PersistentUploadedFile
    instead of TemporaryUploadedFile
    
    I created these classes for a very specific reason, that relates to my
    using Dropzone.js to upload files. I could only get Dropzone to work by
    making an Ajax request to send the files, separate from the transfer
    form's POST submission. It so happened in my view functions that the
    temporary files were being deleted after they were processed in the
    Ajax request on the Django backend. I needed the files to stay on the
    file system until the POST request came through for the form, hence the
    delete=False flag. Obviously, this is a less safe way to create temp
    files since they are not removed automatically, but it should be
    possible in the future to periodically clean the temporary upload folder
    to make sure there are no persistent storage memory leaks. At this
    point, I am removing the temp files after the Bagger finishes creating a
    bag with the files.

---
+++ bagitobjecttransfer/mockup/persistentuploadhandler.py
@@ -0,0 +1,50 @@
+from django.core.files.uploadhandler import FileUploadHandler
+from django.core.files.uploadedfile import UploadedFile
+from django.conf import settings
+
+import os
+import tempfile
+
+
+class PersistentUploadedFile(UploadedFile):
+    """
+    A file uploaded to a temporary location that must be removed manually.
+    """
+    def __init__(self, name, content_type, size, charset, content_type_extra=None):
+        _, ext = os.path.splitext(name)
+        temporaryfile = tempfile.NamedTemporaryFile(suffix='.upload' + ext, dir=settings.FILE_UPLOAD_TEMP_DIR, delete=False)
+        super().__init__(temporaryfile, name, content_type, size, charset, content_type_extra)
+
+    def temporary_file_path(self):
+        return self.file.name
+
+    def close(self):
+        try:
+            return self.file.close()
+        except FileNotFoundError:
+            # The file was moved or deleted before the tempfile could unlink
+            # it. Still sets self.file.close_called and calls
+            # self.file.file.close() before the exception.
+            pass
+
+
+class PersistentFileUploadHandler(FileUploadHandler):
+    """
+    Upload handler that streams data into a temporary file that must be removed
+    manually.
+    """
+    def new_file(self, *args, **kwargs):
+        """
+        Create the file object to append to as data is coming in.
+        """
+        super().new_file(*args, **kwargs)
+        self.file = PersistentUploadedFile(self.file_name, self.content_type, 0,
+            self.charset, self.content_type_extra)
+
+    def receive_data_chunk(self, raw_data, start):
+        self.file.write(raw_data)
+
+    def file_complete(self, file_size):
+        self.file.seek(0)
+        self.file.size = file_size
+        return self.file
