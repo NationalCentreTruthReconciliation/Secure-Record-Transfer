3463a1e5f697980b42fceb723c7b74ebf90a366f
Daniel Lovegrove <d.lovegrove11@gmail.com>
2020-03-13 11:30:06 -0500

    Register new models in admin

--- bagitobjecttransfer/mockup/admin.py
+++ bagitobjecttransfer/mockup/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import AppUser, Bag
+from .models import AppUser, Bag, UploadSession, UploadedFile
 
 admin.site.register(AppUser)
 admin.site.register(Bag)
+admin.site.register(UploadedFile)
+admin.site.register(UploadSession)
