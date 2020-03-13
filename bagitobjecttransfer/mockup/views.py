824d22fd78a20397c7da94f19fc92fcd1c4450b6
Daniel Lovegrove <d.lovegrove11@gmail.com>
2020-03-13 11:33:41 -0500

    Totally switch bagging to new token-based approach

--- bagitobjecttransfer/mockup/views.py
+++ bagitobjecttransfer/mockup/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
-from django.urls import reverse
-from django.views.generic import TemplateView, FormView
+""" Views
+"""
 
 from json.decoder import JSONDecodeError
 import json
 import logging
 import os
 import threading
+
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.urls import reverse
+from django.views.generic import TemplateView, FormView
 
 from mockup.appsettings import BAG_STORAGE_FOLDER
 from mockup.bagger import Bagger
@@ -42,22 +45,29 @@ def uploadfiles(request):
         return JsonResponse({'files': []}, status=200)
 
     try:
+        headers = request.headers
+        if not 'Upload-Session-Token' in headers or not headers['Upload-Session-Token']:
+            session = UploadSession.new_session()
+            session.save()
+        else:
+            session = UploadSession.objects.filter(token=headers['Upload-Session-Token']).first()
+            if session is None:
+                session = UploadSession.new_session()
+                session.save()
+
         files_dictionary = request.FILES.dict()
-
-        session = UploadSession()
-        session.save()
-
         for key in files_dictionary:
             if not isinstance(files_dictionary[key], PersistentUploadedFile):
                 return JsonResponse({
-                    'error': "Development issue: Wrong file upload handler being used, check Django" \
-                   f" settings. Got file of type {type(files_dictionary[key]).__name__}, but" \
-                    " expected PersistentUploadedFile.",
+                    'error': "Development issue: Wrong file upload handler being used, check"
+                    f" Django settings. Got file of type {type(files_dictionary[key]).__name__},"
+                    "but expected PersistentUploadedFile.",
                 }, status=500)
 
             file_path = files_dictionary[key].temporary_file_path()
             file_name = files_dictionary[key].name
-            new_file = UploadedFile(name=file_name, path=file_path, old_copy_removed=False, session=session)
+            new_file = UploadedFile(name=file_name, path=file_path, old_copy_removed=False,
+                                    session=session)
             new_file.save()
 
         return JsonResponse({'upload_session_token': session.token}, status=200)
@@ -71,8 +81,7 @@ def sendtransfer(request):
         try:
             form = TransferForm(request.POST)
             if form.is_valid():
-                file_list_json = form.cleaned_data['file_list_json']
-                uploaded_files = json.loads(file_list_json)
+                session_token = form.cleaned_data['session_token']
                 first_name = form.cleaned_data['first_name']
                 last_name = form.cleaned_data['last_name']
                 metadata = {
@@ -86,7 +95,7 @@ def sendtransfer(request):
                 logger.info('Views: Starting bag creation in the background')
                 bagging_thread = threading.Thread(
                     target=create_bag_background,
-                    args=(BAG_STORAGE_FOLDER, uploaded_files, metadata)
+                    args=(BAG_STORAGE_FOLDER, session_token, metadata)
                 )
                 bagging_thread.setDaemon(True)
                 bagging_thread.start()
@@ -94,18 +103,20 @@ def sendtransfer(request):
             else:
                 for err in form.errors:
                     for message in form.errors[err]:
-                        print (f'{err} Error: {message}')
+                        print(f'{err} Error: {message}')
 
-                if form.data['file_list_json']:
-                    files = json.loads(form.data['file_list_json'])
-                    remove_file_list(files)
+                if form.data['session_token']:
+                    uploaded_files = UploadedFile.objects.filter(
+                        session__token=form.data['session_token'])
+                    for upload in uploaded_files:
+                        upload.delete_file()
 
                 return render(request, 'mockup/transfer.html', {'form': form})
-        except JSONDecodeError as e:
-            print(f'JSONDecodeError: {e}')
+        except JSONDecodeError as exc:
+            print(f'JSONDecodeError: {exc}')
             return render(request, 'mockup/transfer.html', {'form': form})
-        except KeyError as e:
-            print(f'KeyError: {e}')
+        except KeyError as exc:
+            print(f'KeyError: {exc}')
             return render(request, 'mockup/transfer.html', {'form': form})
         else:
             return HttpResponseRedirect(reverse('mockup:transfersent'))
@@ -122,11 +133,5 @@ def remove_file_list(file_list: list):
             pass
 
 
-def create_bag_background(storage_folder: str, files: list, metadata: dict):
-    Bagger.create_bag(storage_folder, files, metadata)
-
-    for f in files:
-        try:
-            os.remove(f['filepath'])
-        except FileNotFoundError:
-            logging.warn(f'Views: It appears that file {f["filepath"]} has already been removed')
+def create_bag_background(storage_folder: str, upload_session_token: list, metadata: dict):
+    Bagger.create_bag(storage_folder, upload_session_token, metadata, deletefiles=True)
