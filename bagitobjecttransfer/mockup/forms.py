e19bc849a308d71d46b8d74846e380fdfa541b3c
Daniel Lovegrove <d.lovegrove11@gmail.com>
2020-03-18 10:20:33 -0500

    Start adding forms to conform to CAAIS

--- bagitobjecttransfer/mockup/forms.py
+++ bagitobjecttransfer/mockup/forms.py
@@ -1,6 +1,86 @@
 from django import forms
 
 
+class SourceForm(forms.Form):
+    source_name = forms.CharField(
+        max_length=64,
+        min_length=2,
+        required=True,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'The organization or entity submitting the records'
+        }),
+    )
+
+    source_type = forms.CharField(
+        max_length=64,
+        min_length=2,
+        required=False,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'The kind of entity the source is (optional)'
+        }),
+    )
+
+    source_role = forms.ChoiceField(
+        required=True,
+        choices=[
+            'Creator',
+            'Donor',
+            'Custodian',
+            'Unknown',
+        ],
+        widget=forms.Select,
+    )
+
+    source_note = forms.CharField(
+        required=False,
+        widget=forms.Textarea(attrs={
+            'rows': '4',
+            'placeholder': 'Enter any notes you have on how the source relates to the records'
+                           ' (optional)'
+        }),
+    )
+
+    custodial_history = forms.CharField(
+        required=False,
+        widget=forms.Textarea(attrs={
+            'rows': '4',
+            'placeholder': 'Enter any notes you have on how the custodial history of the records'
+                           ' (optional)'
+        }),
+    )
+
+class ContactInfoForm(forms.Form):
+    contact_name = forms.CharField(
+        max_length=64,
+        min_length=2,
+        required=True,
+        widget=forms.TextInput(
+            attrs={'placeholder': 'Enter your name'}
+        ),
+    )
+
+    job_title = forms.CharField(
+        max_length=64,
+        min_length=2,
+        required=True,
+        widget=forms.TextInput(
+            attrs={'placeholder': 'Enter your job title'}
+        )
+    )
+
+    phone_number = forms.RegexField(
+        regex=r'^\+\d\s\(\d{3}\)\s\d{3}-\d{4}$',
+        error_messages={
+            'required': 'This field is required.',
+            'invalid': 'Phone number must look like "+1 (999) 999-9999"'
+        },
+        widget=forms.TextInput(attrs={'placeholder': '+1 (999) 999-9999'},)
+    )
+
+    email = forms.EmailField(
+        widget=forms.TextInput(attrs={'placeholder': 'Enter your email'}),
+    )
+
 class TransferForm(forms.Form):
     first_name = forms.CharField(
         max_length=32,
