900bc9d61dd42dd4eaaebdf404a8577c98ae3c61
Daniel Lovegrove <d.lovegrove11@gmail.com>
2020-03-26 10:33:39 -0500

    Add new date input mask

--- bagitobjecttransfer/mockup/static/mockup/js/inputmasks.js
+++ bagitobjecttransfer/mockup/static/mockup/js/inputmasks.js
@@ -2,4 +2,8 @@ $(function() {
     $('input[id$="phone_number"').each(function() {
         $(this).mask('+0 (000) 000-0000')
     })
+
+    $('input[id$="date_of_material"').each(function() {
+        $(this).mask('0000-00-00')
+    })
 })
