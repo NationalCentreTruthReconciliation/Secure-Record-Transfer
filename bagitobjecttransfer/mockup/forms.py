ed0fcef614a7420266eda4f64c4b243926710e05
Daniel Lovegrove <d.lovegrove11@gmail.com>
2020-03-26 12:24:34 -0500

    Override form clean method to check end date after start

--- bagitobjecttransfer/mockup/forms.py
+++ bagitobjecttransfer/mockup/forms.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+
 from django import forms
 from django_countries.fields import CountryField
 
-from mockup.validators import validate_date
+from mockup.validators import validate_date, FULL_DATE, SINGLE_YEAR
 
 
 class SourceInfoForm(forms.Form):
@@ -136,6 +138,60 @@ class ContactInfoForm(forms.Form):
 
 
 class RecordDescriptionForm(forms.Form):
+    def clean(self):
+        cleaned_data = super().clean()
+
+        start_date = cleaned_data.get('start_date_of_material')
+        end_date = cleaned_data.get('end_date_of_material')
+
+        # Check end date after start date
+        if start_date and end_date:
+            start_year_only = False
+            start_date_year_match = SINGLE_YEAR.match(start_date)
+            if start_date_year_match:
+                start_year_only = True
+                start_year = int(start_date_year_match.group('year'))
+                start_month = 0
+                start_date = 0
+            else:
+                start_date_full_match = FULL_DATE.match(start_date)
+                start_year = int(start_date_full_match.group('year'))
+                start_month = int(start_date_full_match.group('month'))
+                start_date = int(start_date_full_match.group('date'))
+
+            end_year_only = False
+            end_date_year_match = SINGLE_YEAR.match(end_date)
+            if end_date_year_match:
+                end_year_only = True
+                end_year = int(end_date_year_match)
+                end_month = 0
+                end_date = 0
+            else:
+                end_date_full_match = FULL_DATE.match(end_date)
+                end_year = int(end_date_full_match.group('year'))
+                end_month = int(end_date_full_match.group('month'))
+                end_date = int(end_date_full_match.group('date'))
+
+            # Fix zero-ed months and dates
+            if start_year_only and end_year_only:
+                start_month = start_date = end_month = end_date = 1
+            elif start_year_only and not end_year_only:
+                start_month, start_date = end_month, end_date
+            elif not start_year_only and end_year_only:
+                end_month, end_date = start_month, start_date
+
+            start_datetime = datetime(start_year, start_month, start_date)
+            end_datetime = datetime(end_year, end_month, end_date)
+
+            if end_datetime == start_datetime:
+                msg = 'Start and end dates are equivalent. Change one of them'
+                self.add_error('start_date_of_material', msg)
+                self.add_error('end_date_of_material', msg)
+            elif end_datetime < start_datetime:
+                msg = 'End date cannot be before start date'
+                self.add_error('end_date_of_material', msg)
+
+
     collection_title = forms.CharField(
         min_length=2,
         max_length=100,
@@ -160,58 +216,31 @@ class RecordDescriptionForm(forms.Form):
         help_text='Enter either a single year or a yyyy-mm-dd formatted date'
     )
 
-
-class UploadFilesForm(forms.Form):
-    session_token = forms.CharField(widget=forms.HiddenInput())
-
-
-class TransferForm(forms.Form):
-    first_name = forms.CharField(
-        max_length=32,
-        min_length=2,
-        widget=forms.TextInput(attrs={'placeholder': 'Enter your first name'}),
+    start_date_is_approximate = forms.BooleanField(
+        required=False,
+        widget=forms.CheckboxInput(),
+        help_text='Check the box if the start date is an estimate or a guess'
     )
 
-    last_name = forms.CharField(
-        max_length=32,
-        min_length=2,
-        widget=forms.TextInput(attrs={'placeholder': 'Enter your last name'}),
-    )
-
-    phone_number = forms.RegexField(
-        regex=r'^\+\d\s\(\d{3}\)\s\d{3}-\d{4}$',
+    end_date_of_material = forms.RegexField(
+        regex=r'^(?:\d{4})|(?:\d{4})-(?:\d{2})-(?:\d{2})$',
+        required=True,
         error_messages={
             'required': 'This field is required.',
-            'invalid': 'Phone number must look like "+1 (999) 999-9999"'
         },
-        widget=forms.TextInput(attrs={'placeholder': '+1 (999) 999-9999'},)
-    )
-
-    email = forms.EmailField(
-        widget=forms.TextInput(attrs={'placeholder': 'Enter your email'}),
-    )
-
-    organization = forms.CharField(
-        max_length=128,
-        min_length=2,
-        required=False,
-        widget=forms.TextInput(attrs={'placeholder': 'Enter your organization (optional)'}),
-    )
-
-    organization_address = forms.CharField(
-        max_length=256,
-        min_length=8,
-        required=False,
         widget=forms.TextInput(attrs={
-            'placeholder': 'Enter your organization\'s address (optional)'
+            'placeholder': '2000-12-31'
         }),
+        validators=[validate_date],
+        help_text='Enter either a single year or a yyyy-mm-dd formatted date'
     )
 
-    description = forms.CharField(
-        widget=forms.Textarea(attrs={
-            'rows': '4',
-            'placeholder': 'Enter a brief description of your uploaded files',
-        }),
+    end_date_is_approximate = forms.BooleanField(
+        required=False,
+        widget=forms.CheckboxInput(),
+        help_text='Check the box if the end date is an estimate or a guess'
     )
 
+
+class UploadFilesForm(forms.Form):
     session_token = forms.CharField(widget=forms.HiddenInput())
