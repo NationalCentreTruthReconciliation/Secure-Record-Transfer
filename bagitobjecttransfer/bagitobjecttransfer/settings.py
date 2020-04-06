1195ed8103d951ccf2d906e91dac50be7fdeed71
Daniel Lovegrove <d.lovegrove11@gmail.com>
2020-04-06 10:16:56 -0500

    Start adding a login page

--- bagitobjecttransfer/bagitobjecttransfer/settings.py
+++ bagitobjecttransfer/bagitobjecttransfer/settings.py
@@ -59,7 +59,9 @@ ROOT_URLCONF = 'bagitobjecttransfer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
