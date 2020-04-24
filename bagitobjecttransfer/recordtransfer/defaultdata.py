daee2c8dc67eb8eeef3f0fa63d0d33c0a6f173a7
Daniel Lovegrove <d.lovegrove11@gmail.com>
2020-04-24 10:34:38 -0500

    Add default data string file

---
+++ bagitobjecttransfer/recordtransfer/defaultdata.py
@@ -0,0 +1,33 @@
+# Default data for BagIt tags and accession document generation
+DEFAULT_DATA = {
+    'section_1': {
+        'repository': 'NCTR',
+        'archival_unit': 'NCTR Archives',
+        'acqusition_method': 'Digital Transfer',
+    },
+    'section_2': {
+    },
+    'section_3': {
+        'extent_statement_type': 'Extent received',
+        'extent_statement_note': 'Files counted automatically by application',
+    },
+    'section_4': {
+        'storage_location': 'N/A',
+        'material_assessment_statement': 'N/A',
+        'appraisal_statement': 'N/A',
+        'associated_documentation': 'N/A',
+    },
+    'section_5': {
+        'event_type': 'Digital Object Transfer',
+        'event_agent': 'NCTR Digital Object Transfer Application',
+    },
+    'section_6': {
+        'general_notes': 'N/A',
+    },
+    'section_7': {
+        'rules_or_conventions': 'Canadian Archival Accession Information Standards v1.0',
+        'action_type': 'Created',
+        'action_agent': 'N/A',
+        'action_note': 'Created with NCTR Record Transfer Portal',
+    }
+}
