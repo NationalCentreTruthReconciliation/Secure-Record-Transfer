b291f457b9066d9f4fcbebf4219b9019958d755f
Daniel Lovegrove <d.lovegrove11@gmail.com>
2020-09-01 16:02:15 -0500

    Move login_required to urls.py

--- bagitobjecttransfer/recordtransfer/urls.py
+++ bagitobjecttransfer/recordtransfer/urls.py
@@ -1,22 +1,23 @@
 from django.urls import path
+from django.forms import formset_factory
+from django.contrib.auth.decorators import login_required
 
 from . import views
 from . import forms
-from django.forms import formset_factory
 
 app_name = 'recordtransfer'
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
-    path('pretransfer', views.FormPreparation.as_view(), name='formpreparation'),
-    path('transfer/', views.TransferFormWizard.as_view([
-            ('sourceinfo', forms.SourceInfoForm),
-            ('contactinfo', forms.ContactInfoForm),
-            ('recorddescription', forms.RecordDescriptionForm),
-            ('rights', formset_factory(forms.RightsForm, extra=1)),
-            ('otheridentifiers', formset_factory(forms.OtherIdentifiersForm, extra=1)),
-            ('uploadfiles', forms.UploadFilesForm),
-        ]
-    ), name='transfer'),
-    path('transfer/uploadfile/', views.uploadfiles, name='uploadfile'),
+    path('pretransfer/', login_required(views.FormPreparation.as_view()),
+         name='formpreparation'),
+    path('transfer/', login_required(views.TransferFormWizard.as_view([
+        ('sourceinfo', forms.SourceInfoForm),
+        ('contactinfo', forms.ContactInfoForm),
+        ('recorddescription', forms.RecordDescriptionForm),
+        ('rights', formset_factory(forms.RightsForm, extra=1)),
+        ('otheridentifiers', formset_factory(forms.OtherIdentifiersForm, extra=1)),
+        ('uploadfiles', forms.UploadFilesForm),
+        ])), name='transfer'),
+    path('transfer/uploadfile/', login_required(views.uploadfiles), name='uploadfile'),
     path('transfer/sent/', views.TransferSent.as_view(), name='transfersent'),
 ]
