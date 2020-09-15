e4da52cc3bfa0ddef89c60c8d41e4615940095c2
Daniel Lovegrove <Daniel.Lovegrove@umanitoba.ca>
2020-09-15 16:12:43 -0500

    Fix issue with module path

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ import os
 import sys
 import django
 
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../bagitobjecttransfer'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'bagitobjecttransfer.settings'
 django.setup()
 
