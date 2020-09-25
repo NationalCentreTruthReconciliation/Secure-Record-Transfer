a20cd6f1975e8e0497da6f8730a1f628842e63f4
Daniel Lovegrove <Daniel.Lovegrove@umanitoba.ca>
2020-09-25 10:56:58 -0500

    Add translation support for forms

--- bagitobjecttransfer/recordtransfer/forms.py
+++ bagitobjecttransfer/recordtransfer/forms.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from django import forms
 from django_countries.fields import CountryField
+from django.utils.translation import gettext
 
 from recordtransfer.validators import validate_date, FULL_DATE, SINGLE_YEAR
 
@@ -12,8 +13,9 @@ class SourceInfoForm(forms.Form):
         min_length=2,
         required=True,
         widget=forms.TextInput(attrs={
-            'placeholder': 'The organization or entity submitting the records'
+            'placeholder': gettext('The organization or entity submitting the records')
         }),
+        label=gettext('Source name'),
     )
 
     source_type = forms.CharField(
@@ -21,39 +23,42 @@ class SourceInfoForm(forms.Form):
         min_length=2,
         required=False,
         widget=forms.TextInput(attrs={
-            'placeholder': 'The kind of entity the source is (optional)'
+            'placeholder': gettext('The kind of entity the source is (optional)')
         }),
+        label=gettext('Source type'),
     )
 
     source_role = forms.ChoiceField(
         required=True,
         choices=[
             (c, c) for c in [
-                'Creator',
-                'Donor',
-                'Custodian',
-                'Other',
-                'Unknown',
+                gettext('Creator'),
+                gettext('Donor'),
+                gettext('Custodian'),
+                gettext('Other'),
+                gettext('Unknown'),
             ]
         ],
         widget=forms.Select,
+        label=gettext('Source role'),
     )
 
     source_note = forms.CharField(
         required=False,
         widget=forms.Textarea(attrs={
             'rows': '4',
-            'placeholder': 'Enter any notes you have on how the source relates to the records'
-                           ' (optional)'
+            'placeholder': gettext('Enter any notes you have on how the source relates to the '
+                                   'records (optional)')
         }),
+        label=gettext('Source note'),
     )
 
     custodial_history = forms.CharField(
         required=False,
         widget=forms.Textarea(attrs={
             'rows': '4',
-            'placeholder': 'Enter any notes you have on the custodial history of the records'
-                           ' (optional)'
+            'placeholder': gettext('Enter any notes you have on the custodial history of the '
+                                   'records (optional)')
         }),
     )
 
@@ -64,8 +69,9 @@ class ContactInfoForm(forms.Form):
         min_length=2,
         required=True,
         widget=forms.TextInput(attrs={
-            'placeholder': 'Enter your name'
+            'placeholder': gettext('Enter your name')
         }),
+        label=gettext('Contact name'),
     )
 
     job_title = forms.CharField(
@@ -73,42 +79,47 @@ class ContactInfoForm(forms.Form):
         min_length=2,
         required=True,
         widget=forms.TextInput(attrs={
-            'placeholder': 'Enter your job title'
+            'placeholder': gettext('Enter your job title')
         }),
+        label=gettext('Job title'),
     )
 
     phone_number = forms.RegexField(
         regex=r'^\+\d\s\(\d{3}\)\s\d{3}-\d{4}$',
         error_messages={
-            'required': 'This field is required.',
-            'invalid': 'Phone number must look like "+1 (999) 999-9999"'
+            'required': gettext('This field is required.'),
+            'invalid': gettext('Phone number must look like "+1 (999) 999-9999"')
         },
         widget=forms.TextInput(attrs={
             'placeholder': '+1 (999) 999-9999'
         }),
-        help_text='Phone number should look like "+1 (123) 456-7890"'
+        help_text=gettext('Phone number should look like "+1 (123) 456-7890"'),
+        label=gettext('Phone number'),
     )
 
     email = forms.EmailField(
         widget=forms.TextInput(attrs={
-            'placeholder': 'Enter your email'
+            'placeholder': gettext('Enter your email')
         }),
+        label=gettext('Email'),
     )
 
     address_line_1 = forms.CharField(
         max_length=100,
         required=True,
         widget=forms.TextInput(attrs={
-            'placeholder': 'Street, and street number'
+            'placeholder': gettext('Street, and street number')
         }),
+        label=gettext('Address line 1'),
     )
 
     address_line_2 = forms.CharField(
         max_length=100,
         required=False,
         widget=forms.TextInput(attrs={
-            'placeholder': 'Unit Number, RPO, PO BOX... (optional)'
+            'placeholder': gettext('Unit Number, RPO, PO BOX... (optional)')
         }),
+        label=gettext('Address line 2'),
     )
 
     province_or_state = forms.ChoiceField(
@@ -116,7 +127,7 @@ class ContactInfoForm(forms.Form):
         choices=[
             (c, c) for c in [
                 # For non-Canadian or non-US addresses
-                "Other",
+                gettext("Other"),
                 # Canadian Provinces
                 "AB", "BC", "MB", "NB", "NL", "NS", "NT", "NU", "ON", "PE", "QC",
                 "SK", "YT",
@@ -128,14 +139,23 @@ class ContactInfoForm(forms.Form):
                 "UT", "VT", "VA", "WA", "WV", "WI", "WY"
             ]
         ],
-        widget=forms.Select
+        widget=forms.Select,
     )
 
-    postal_or_zip_code = forms.CharField(
-        min_length=4
+    postal_or_zip_code = forms.RegexField(
+        regex=r'^(?:[0-9]{5}(?:-[0-9]{4})?)|(?:[A-Za-z]\d[A-Za-z][\- ]?\d[A-Za-z]\d)$',
+        error_messages={
+            'required': gettext('This field is required.'),
+            'invalid': gettext('Postal code must look like "Z0Z 0Z0", zip code must look like '
+                               '"12345" or "12345-1234"')
+        },
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Z0Z 0Z0'
+        }),
+        label=gettext('Postal / Zip code'),
     )
 
-    country = CountryField(blank_label='Select your Country').formfield()
+    country = CountryField(blank_label=gettext('Select your Country')).formfield()
 
 
 class RecordDescriptionForm(forms.Form):
@@ -180,22 +200,18 @@ class RecordDescriptionForm(forms.Form):
             start_datetime = datetime(start_year, start_month, start_date)
             end_datetime = datetime(end_year, end_month, end_date)
 
-            if end_datetime == start_datetime:
-                msg = 'Start and end dates are equivalent. Change one of them'
-                self.add_error('start_date_of_material', msg)
-                self.add_error('end_date_of_material', msg)
-            elif end_datetime < start_datetime:
+            if end_datetime < start_datetime:
                 msg = 'End date cannot be before start date'
                 self.add_error('end_date_of_material', msg)
 
-
-    collection_title = forms.CharField(
+    accession_title = forms.CharField(
         min_length=2,
         max_length=100,
         required=True,
         widget=forms.TextInput(attrs={
-            'placeholder': 'Create a title for this collection of records'
+            'placeholder': gettext('Create a title for this collection of records')
         }),
+        label=gettext('Collection title')
     )
 
     start_date_of_material = forms.RegexField(
@@ -204,56 +220,62 @@ class RecordDescriptionForm(forms.Form):
         regex=r'^(?:\d{4})|(?:\d{4})-(?:\d{2})-(?:\d{2})$',
         required=True,
         error_messages={
-            'required': 'This field is required.',
+            'required': gettext('This field is required.'),
         },
         widget=forms.TextInput(attrs={
             'placeholder': '2000-01-01'
         }),
         validators=[validate_date],
-        help_text='Enter either a single year or a yyyy-mm-dd formatted date'
+        help_text=gettext('Enter either a single year (yyyy) or a yyyy-mm-dd formatted date'),
+        label=gettext('Start date of files'),
     )
 
     start_date_is_approximate = forms.BooleanField(
         required=False,
         widget=forms.CheckboxInput(),
-        help_text='Check the box if the start date is an estimate or a guess'
+        help_text=gettext('Check the box if the start date is an estimate or a guess'),
+        label=gettext('Start date estimated'),
     )
 
     end_date_of_material = forms.RegexField(
         regex=r'^(?:\d{4})|(?:\d{4})-(?:\d{2})-(?:\d{2})$',
         required=True,
         error_messages={
-            'required': 'This field is required.',
+            'required': gettext('This field is required.'),
         },
         widget=forms.TextInput(attrs={
             'placeholder': '2000-12-31'
         }),
         validators=[validate_date],
-        help_text='Enter either a single year or a yyyy-mm-dd formatted date'
+        help_text=gettext('Enter either a single year or a yyyy-mm-dd formatted date'),
+        label=gettext('End date of files'),
     )
 
     end_date_is_approximate = forms.BooleanField(
         required=False,
         widget=forms.CheckboxInput(),
-        help_text='Check the box if the end date is an estimate or a guess'
+        help_text=gettext('Check the box if the end date is an estimate or a guess'),
+        label=gettext('End date estimated'),
     )
 
     language_of_material = forms.CharField(
         required=True,
         min_length=2,
         widget=forms.TextInput(attrs={
-            'placeholder': 'English, French'
+            'placeholder': gettext('English, French')
         }),
-        help_text='Enter all relevant languages here'
+        help_text=gettext('Enter all relevant languages here'),
+        label=gettext('Language of material')
     )
 
-    description = forms.CharField(
+    scope_and_content = forms.CharField(
         required=True,
         min_length=4,
         widget=forms.Textarea(attrs={
             'rows': '6',
-            'placeholder': 'Briefly describe the scope and content of the files to be transferred'
+            'placeholder': gettext('Briefly describe the content of the files you are transferring')
         }),
+        label=gettext('Description'),
     )
 
 
@@ -261,27 +283,31 @@ class RightsForm(forms.Form):
     rights_type = forms.CharField(
         required=True,
         widget=forms.TextInput(attrs={
-            'placeholder': 'Enter the type of rights governing the records',
+            'placeholder': gettext('Enter the type of rights governing the records'),
             'required': 'required',
         }),
-        help_text='For example: "Copyright," "Cultural rights," etc.',
+        help_text=gettext('For example: "Copyright," "Cultural rights," etc.'),
+        label=gettext('Type of rights'),
     )
 
     rights_statement = forms.CharField(
         required=True,
         widget=forms.TextInput(attrs={
-            'placeholder': 'Nature and duration of permission or restrictions',
+            'placeholder': gettext('Nature and duration of permission or restrictions'),
             'required': 'required',
         }),
-        help_text='For example: "Public domain," "FIPPA," "Copyright lasts until 2050," etc.',
+        help_text=gettext('For example: "Public domain," "FIPPA," "Copyright until 2050," etc.'),
+        label=gettext('Description of rights'),
     )
 
     rights_note = forms.CharField(
         required=False,
         widget=forms.Textarea(attrs={
             'rows': '2',
-            'placeholder': 'Any notes on these rights or which files they may apply to (optional).',
+            'placeholder': gettext('Any notes on these rights or which files they may apply to '
+                                   '(optional).'),
         }),
+        label=gettext('Notes for rights')
     )
 
 
@@ -290,12 +316,12 @@ class OtherIdentifiersForm(forms.Form):
         cleaned_data = super().clean()
 
         id_type = cleaned_data.get('other_identifier_type')
-        id_value = cleaned_data.get('identifier_value')
-        id_note = cleaned_data.get('identifier_note')
+        id_value = cleaned_data.get('other_identifier_value')
+        id_note = cleaned_data.get('other_identifier_note')
 
         if id_type and not id_value:
             value_msg = 'Must enter a value for this identifier'
-            self.add_error('identifier_value', value_msg)
+            self.add_error('other_identifier_value', value_msg)
         elif not id_type and id_value:
             type_msg = 'Must enter a type for this identifier'
             self.add_error('other_identifier_type', type_msg)
@@ -303,33 +329,48 @@ class OtherIdentifiersForm(forms.Form):
             type_msg = 'Must enter a type for this identifier'
             self.add_error('other_identifier_type', type_msg)
             value_msg = 'Must enter a value for this identifier'
-            self.add_error('identifier_value', value_msg)
+            self.add_error('other_identifier_value', value_msg)
             note_msg = 'Cannot enter a note without entering a value and type'
-            self.add_error('identifier_note', note_msg)
+            self.add_error('other_identifier_note', note_msg)
 
     other_identifier_type = forms.CharField(
         required=False,
         widget=forms.TextInput(attrs={
-            'placeholder': 'The type of the identifier',
+            'placeholder': gettext('The type of the identifier'),
         }),
-        help_text='For example: "Receipt number", "LAC Record ID", etc.',
+        help_text=gettext('For example: "Receipt number", "LAC Record ID", etc.'),
+        label=gettext('Type of identifier'),
     )
 
-    identifier_value = forms.CharField(
+    other_identifier_value = forms.CharField(
         required=False,
         widget=forms.TextInput(attrs={
-            'placeholder': 'Identifier value',
+            'placeholder': gettext('Identifier value'),
         }),
+        label=gettext('Identifier value'),
     )
 
-    identifier_note = forms.CharField(
+    other_identifier_note = forms.CharField(
         required=False,
         widget=forms.Textarea(attrs={
             'rows': '2',
-            'placeholder': 'Any notes on this identifier or which files it may apply to (optional).',
+            'placeholder': gettext('Any notes on this identifier or which files it may apply to '
+                                   '(optional).')
         }),
+        label=gettext('Notes for identifier')
     )
 
 
 class UploadFilesForm(forms.Form):
     session_token = forms.CharField(widget=forms.HiddenInput())
+
+    general_note = forms.CharField(
+        required=False,
+        min_length=4,
+        widget=forms.Textarea(attrs={
+            'rows': '6',
+            'placeholder': gettext('Put any other notes you would like to add for this transfer '
+                                   'here (optional)')
+        }),
+        label=gettext('Other notes'),
+    )
