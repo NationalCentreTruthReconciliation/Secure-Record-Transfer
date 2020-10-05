2a3eb82ee6167229b094894de1f952f5d3a76127
Daniel Lovegrove <Daniel.Lovegrove@umanitoba.ca>
2020-10-05 13:18:30 -0500

    Use localhost email server (currently using Papercut SMTP)

--- bagitobjecttransfer/bagitobjecttransfer/settings/development.py
+++ bagitobjecttransfer/bagitobjecttransfer/settings/development.py
@@ -32,7 +32,13 @@ RQ_QUEUES = {
     },
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = '127.0.0.1'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
 
 LOGGING = {
     'version': 1,
