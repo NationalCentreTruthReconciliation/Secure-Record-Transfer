51d8097c4d6f59d6dea824944b11fb4d0032b85b
Daniel Lovegrove <d.lovegrove11@gmail.com>
2021-04-14 15:57:00 -0500

    Add redundant file extension check
    If a malicious user manages to circumvent the initial file extension
    check, it is checked again during upload and the endpoint will not allow
    the file to be uploaded.
    
    I check the extension initially so that there's no partially-submitted
    set of files, but if someone bypasses the initial checks, a
    partially-submitted set of files is likely to occur if any one file is
    invalid. This happens if one or more files are uploaded succesfully
    before an invalid one is attempted to be uploaded.

--- bagitobjecttransfer/recordtransfer/static/recordtransfer/js/forms.js
+++ bagitobjecttransfer/recordtransfer/static/recordtransfer/js/forms.js
@@ -254,7 +254,8 @@ $(() => {
     /***************************************************************************
      * Dropzone Setup
      **************************************************************************/
-    var issueFiles = []
+    var issueFiles = []    // An array of file names
+    var uploadedFiles = [] // An array of File objects
     var sessionToken = ''
     const csrfToken = getCookie('csrftoken')
 
@@ -302,25 +303,34 @@ $(() => {
         },
         init: function() {
             issueFiles = []
-            dropzoneClosure = this
-            submitButton = document.getElementById("submit-form-btn")
+            uploadedFiles = []
+            sessionToken = ''
+
+            var dropzoneClosure = this
+            var submitButton = document.getElementById("submit-form-btn")
 
             submitButton.addEventListener("click", (event) => {
                 event.preventDefault()
                 event.stopPropagation()
-                clearDropzoneErrors()
 
-                if (dropzoneClosure.getQueuedFiles().length > 0) {
-                    dropzoneClosure.options.autoProcessQueue = true
-                    dropzoneClosure.processQueue()
-                } else {
-                    addDropzoneError('You cannot submit a form without files.')
+                if (issueFiles.length > 0) {
+                    alert('One or more files cannot be uploaded. Remove them before submitting')
                 }
-            });
+                else {
+                    clearDropzoneErrors()
+                    if (dropzoneClosure.getQueuedFiles().length + uploadedFiles.length === 0) {
+                        alert('You cannot submit a form without files. Please choose at least one file')
+                    }
+                    else {
+                        dropzoneClosure.options.autoProcessQueue = true
+                        dropzoneClosure.processQueue()
+                    }
+                }
+            })
 
             // Triggers on non-200 status
             this.on("error", (file, response, xhr) => {
-                if ("verboseError" in response) {
+                if (response.verboseError) {
                     addDropzoneError(response.verboseError)
                 }
                 else {
@@ -331,19 +341,15 @@ $(() => {
                 dropzoneClosure.options.autoProcessQueue = false
             })
 
-            this.on("addedfile", (file) => {
-                clearDropzoneErrors()
-            })
-
             this.on("removedfile", (file) => {
                 // If this file previously caused an issue, remove it from issueFiles
                 issueIndex = issueFiles.indexOf(file.name)
                 if (issueIndex > -1) {
                     issueFiles.splice(issueIndex, 1)
                 }
-
                 if (issueFiles.length === 0) {
                     document.getElementById("submit-form-btn").disabled = false
+                    clearDropzoneErrors()
                 }
             })
 
@@ -351,8 +357,24 @@ $(() => {
                 xhr.setRequestHeader("Upload-Session-Token", sessionToken)
             })
 
-            this.on("successmultiple", (file, response) => {
-                sessionToken = response['upload_session_token']
+            this.on("successmultiple", (files, response) => {
+                if (response.uploadSessionToken) {
+                    sessionToken = response.uploadSessionToken
+                }
+                var invalidFileNames = []
+                for (const issue of response.issues) {
+                    var fileName = issue.file
+                    invalidFileNames.push(fileName)
+                    var fileObj = files.filter(file => { return file.name === fileName })[0]
+                    var errMessage = {'error': issue.error, 'verboseError': issue.verboseError}
+                    dropzoneClosure.emit('error', fileObj, errMessage, null)
+                    console.log(`pushed: ${fileName}`)
+                }
+                for (const file of files) {
+                    if (invalidFileNames && invalidFileNames.indexOf(file.name) === -1) {
+                        uploadedFiles.push(file)
+                    }
+                }
             })
 
             this.on("queuecomplete", () => {
@@ -370,7 +392,7 @@ $(() => {
                         console.error('Could not find any input id matching "session_token" on the page!')
                     }
                 }
-                else {
+                else if (!issueFiles.length === dropzoneClosure.files.length) {
                     alert('There are one or more files that could not be uploaded. Remove these files and try again.')
                 }
             })
