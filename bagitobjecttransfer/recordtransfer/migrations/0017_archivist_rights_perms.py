ddb5d383c58fd97019e095e9304e9382db46be2b
Daniel Lovegrove <d.lovegrove11@gmail.com>
2022-03-17 14:29:09 -0500

    Add emit_post_migrate_signal where it's missing

--- bagitobjecttransfer/recordtransfer/migrations/0017_archivist_rights_perms.py
+++ bagitobjecttransfer/recordtransfer/migrations/0017_archivist_rights_perms.py
@@ -1,9 +1,11 @@
 from django.db import migrations
+from django.core.management.sql import emit_post_migrate_signal
 from django.contrib.auth.models import Group, Permission
 
 
 def populate_permissions(apps, schema_editor):
     ''' Add permissions to edit rights to archivist staff '''
+    emit_post_migrate_signal(1, False, 'default')
     group = Group.objects.get(name='archivist_user')
     existing_permissions = group.permissions.all()
 
