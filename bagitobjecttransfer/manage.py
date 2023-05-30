300ef05c3222b143998ba12b4ccde582e3cdc244
Daniel Lovegrove <d.lovegrove11@gmail.com>
2023-05-30 11:42:02 -0500

    Run debugpy for app on port 8009
    debugpy is now set to run when DEBUG is True, and ENABLE_DEBUGPY is set
    to "1". It is still not possible to debug asynchronous jobs from
    jobs.py, it may be necessary to start a separate debugpy instance on an
    adjacent port to provide debugging capabilities on the RQ jobs.

--- bagitobjecttransfer/manage.py
+++ bagitobjecttransfer/manage.py
@@ -5,12 +5,36 @@ import sys
 from decouple import config
 
 
+def initialize_debugger():
+    ''' Start debugging if these conditions are met:
+
+    - The Django DEBUG setting is True
+    - The ENABLE_DEBUGPY environment variable is "1"
+    - This is the main Django process (i.e., this is not the reloader process
+      Django runs to check for code changes)
+
+    You may also set the port debugpy uses. Otherwise, the port 8009 is used.
+
+    See:
+    - https://stackoverflow.com/a/73437126
+    - https://stackoverflow.com/a/62944426
+    '''
+    from django.conf import settings
+
+    if settings.DEBUG and config('ENABLE_DEBUGPY', default='0') == '1' and not os.getenv('RUN_MAIN'):
+        import debugpy
+        port = config('DEBUGPY_PORT', default=8009, cast=int)
+        print(f'Attaching debugger to port {port}')
+        debugpy.listen(('0.0.0.0', port))
+
+
 def main():
     if 'DJANGO_SETTINGS_MODULE' not in os.environ:
         os.environ['DJANGO_SETTINGS_MODULE'] = config(
             'DJANGO_SETTINGS_MODULE',
             default='bagitobjecttransfer.settings.sphinx',
         )
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -19,6 +43,8 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    initialize_debugger()
     execute_from_command_line(sys.argv)
 
 
