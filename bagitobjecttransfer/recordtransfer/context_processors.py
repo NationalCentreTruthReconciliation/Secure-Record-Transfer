b3909884a00fdcff51c2e0aec2d51801d6e67582
Daniel Lovegrove <d.lovegrove11@gmail.com>
2023-07-27 13:14:59 -0500

    Add context processor for templates

--- bagitobjecttransfer/recordtransfer/context_processors.py
+++ bagitobjecttransfer/recordtransfer/context_processors.py
@@ -4,6 +4,9 @@ from recordtransfer.settings import SIGN_UP_ENABLED, MAX_SINGLE_UPLOAD_SIZE, \
 def signup_status(request):
     return {'SIGN_UP_ENABLED': SIGN_UP_ENABLED}
 
+def file_upload_status(request):
+    return {'FILE_UPLOAD_ENABLED': 'FILE_UPLOAD_ENABLED'}
+
 def file_uploads(request):
     return {
         'MAX_TOTAL_UPLOAD_SIZE': MAX_TOTAL_UPLOAD_SIZE,
