f72fe8f948dd8c7f05e03659adf2bf1d5e79e95b
Daniel Lovegrove <d.lovegrove11@gmail.com>
2023-07-28 11:35:43 -0500

    Fix context processor, fix block

--- bagitobjecttransfer/recordtransfer/context_processors.py
+++ bagitobjecttransfer/recordtransfer/context_processors.py
@@ -1,11 +1,11 @@
 from recordtransfer.settings import SIGN_UP_ENABLED, MAX_SINGLE_UPLOAD_SIZE, \
-    MAX_TOTAL_UPLOAD_COUNT, MAX_TOTAL_UPLOAD_SIZE
+    MAX_TOTAL_UPLOAD_COUNT, MAX_TOTAL_UPLOAD_SIZE, FILE_UPLOAD_ENABLED
 
 def signup_status(request):
     return {'SIGN_UP_ENABLED': SIGN_UP_ENABLED}
 
 def file_upload_status(request):
-    return {'FILE_UPLOAD_ENABLED': 'FILE_UPLOAD_ENABLED'}
+    return {'FILE_UPLOAD_ENABLED': FILE_UPLOAD_ENABLED}
 
 def file_uploads(request):
     return {
