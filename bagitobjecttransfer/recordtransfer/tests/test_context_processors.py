4ba765a824592b8f3fcb767eb1e3395f1a7944d3
Kelvin Oo <Kelvin.Oo@umanitoba.ca>
2024-09-25 13:59:38 -0500

    change deprecated assertEquals to assertEqual

--- bagitobjecttransfer/recordtransfer/tests/test_context_processors.py
+++ bagitobjecttransfer/recordtransfer/tests/test_context_processors.py
@@ -7,24 +7,24 @@ from recordtransfer import settings
 class TestSignupStatus(TestCase):
     def test_response_has_signup_status(self):
         response = self.client.get(reverse('recordtransfer:index'))
-        self.assertEquals(response.context['SIGN_UP_ENABLED'], settings.SIGN_UP_ENABLED)
+        self.assertEqual(response.context['SIGN_UP_ENABLED'], settings.SIGN_UP_ENABLED)
 
 
 class TestFileUploadStatus(TestCase):
     def test_response_has_file_upload_status(self):
         response = self.client.get(reverse('recordtransfer:index'))
-        self.assertEquals(response.context['FILE_UPLOAD_ENABLED'], settings.FILE_UPLOAD_ENABLED)
+        self.assertEqual(response.context['FILE_UPLOAD_ENABLED'], settings.FILE_UPLOAD_ENABLED)
 
 
 class TestFileUploadLimit(TestCase):
     def test_response_has_max_total_size(self):
         response = self.client.get(reverse('recordtransfer:index'))
-        self.assertEquals(response.context['MAX_TOTAL_UPLOAD_SIZE'], settings.MAX_TOTAL_UPLOAD_SIZE)
+        self.assertEqual(response.context['MAX_TOTAL_UPLOAD_SIZE'], settings.MAX_TOTAL_UPLOAD_SIZE)
 
     def test_response_has_max_single_size(self):
         response = self.client.get(reverse('recordtransfer:index'))
-        self.assertEquals(response.context['MAX_SINGLE_UPLOAD_SIZE'], settings.MAX_SINGLE_UPLOAD_SIZE)
+        self.assertEqual(response.context['MAX_SINGLE_UPLOAD_SIZE'], settings.MAX_SINGLE_UPLOAD_SIZE)
 
     def test_response_has_max_count(self):
         response = self.client.get(reverse('recordtransfer:index'))
-        self.assertEquals(response.context['MAX_TOTAL_UPLOAD_COUNT'], settings.MAX_TOTAL_UPLOAD_COUNT)
+        self.assertEqual(response.context['MAX_TOTAL_UPLOAD_COUNT'], settings.MAX_TOTAL_UPLOAD_COUNT)
