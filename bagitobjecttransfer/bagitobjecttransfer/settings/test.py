017dcbfb0f1abb1b40707f05cadef52f7e3b87a9
Daniel Lovegrove <d.lovegrove11@gmail.com>
2024-10-01 15:59:12 -0500

    Combine settings sphinx.py & test.py -> test.py
    Combines the two settings files into one that works for both Sphinx and
    for testing.

--- bagitobjecttransfer/bagitobjecttransfer/settings/test.py
+++ bagitobjecttransfer/bagitobjecttransfer/settings/test.py
@@ -1,26 +1,93 @@
-from .sphinx import *
+# Minimal settings for either testing, or building docs with Sphinx
+from .base import *
 
 DEBUG = True
 
-TEST_RUNNER = 'override_storage.LocMemStorageDiscoverRunner'
+TEST_RUNNER = "override_storage.LocMemStorageDiscoverRunner"
+
+SITE_ID = 1
+
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'TEST': {
-            'NAME': os.path.join(BASE_DIR, 'db.test.sqlite3')
-        }
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "TEST": {
+            "NAME": os.path.join(BASE_DIR, "db.test.sqlite3"),
+        },
     }
 }
 
+RQ_QUEUES = {
+    "default": {
+        "HOST": "0.0.0.0",
+        "PORT": 6379,
+        "DB": 0,  # Redis database index
+        "PASSWORD": "",
+        "DEFAULT_TIMEOUT": 500,
+    },
+}
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "0.0.0.0"
+EMAIL_PORT = 1025
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+EMAIL_USE_TLS = False
+
+SILENCED_SYSTEM_CHECKS = ["captcha.recaptcha_test_key_error"]
+
 PASSWORD_HASHERS = [
-    'django.contrib.auth.hashers.MD5PasswordHasher',
+    "django.contrib.auth.hashers.MD5PasswordHasher",
 ]
 
 MIDDLEWARE_CLASSES = [
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
 ]
+
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "standard": {
+            "format": "{levelname} {asctime} {module}: {message}",
+            "style": "{",
+        }
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "ERROR",
+        },
+        "recordtransfer": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+        "rq.worker": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+        "clamav": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+    },
+}
