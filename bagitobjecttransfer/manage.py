c0287ea17cd161c1e40baa3572eb2963f25c64a1
Daniel Lovegrove <d.lovegrove11@gmail.com>
2024-10-17 16:21:32 -0500

    Format manage.py and fix bug

--- bagitobjecttransfer/manage.py
+++ bagitobjecttransfer/manage.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+
 import os
 import sys
+
 from decouple import config
 
 
-def initialize_debugger():
-    ''' Start debugging if these conditions are met:
+def initialize_debugger() -> None:
+    """Start debugger when debugging conditions are met.
+
+    The port used by debugpy is set by DEBUG_PORT.
+
+    The following conditions must be met to start debugging:
 
     - The Django DEBUG setting is True
     - The ENABLE_DEBUGPY environment variable is "1"
@@ -17,43 +23,50 @@ def initialize_debugger():
       "ENABLE_DEBUGPY_ON_COMMAND". This is to avoid clashing with other
       manage.py commands like migrate or shell or makemigrations.
 
-    You may also set the port debugpy uses. Otherwise, the port 8009 is used.
-
     See below for info related to RUN_MAIN:
     - https://stackoverflow.com/a/73437126
     - https://stackoverflow.com/a/62944426
-    '''
+    """
     from django.conf import settings
 
-    if settings.DEBUG and \
-        config('ENABLE_DEBUGPY', default='0') == '1' and \
-        not os.getenv('RUN_MAIN'):
-
-        command = config('ENABLE_DEBUGPY_ON_COMMAND', default='runserver')
+    if (
+        settings.DEBUG
+        and config("ENABLE_DEBUGPY", cast=str, default="0") == "1"
+        and not os.getenv("RUN_MAIN")
+    ):
+        command = config("ENABLE_DEBUGPY_ON_COMMAND", default="runserver")
 
         if command in sys.argv:
             import debugpy
-            port = config('DEBUGPY_PORT', default=8009, cast=int)
-            print(f'Attaching debugger to port {port}')
-            debugpy.listen(('0.0.0.0', port))
+
+            port = config("DEBUGPY_PORT", default=8009, cast=int)
+            print(f"Attaching debugger to port {port}")
+            debugpy.listen(("0.0.0.0", port))
 
 
-def check_yuglify_binary():
+def check_yuglify_binary() -> None:
+    """Check that the yuglify binary exists when !DEBUG.
+
+    Raise ImproperlyConfigured if the binary is missing.
+    """
     from django.conf import settings
     from django.core.exceptions import ImproperlyConfigured
 
-    if settings.DEBUG and not os.path.exists(settings.YUGLIFY_BINARY):
+    if not settings.DEBUG and not os.path.exists(settings.YUGLIFY_BINARY):
         raise ImproperlyConfigured(
             f"Could not find yuglify binary at '{settings.YUGLIFY_BINARY}'. Did you forget "
             "to execute 'npm run compile'?"
         )
 
 
-def main():
+def main() -> None:
+    """Run application with command line arguments."""
     if "DJANGO_SETTINGS_MODULE" not in os.environ:
-        os.environ["DJANGO_SETTINGS_MODULE"] = config(
-            "DJANGO_SETTINGS_MODULE",
-            default="bagitobjecttransfer.settings.test",
+        os.environ["DJANGO_SETTINGS_MODULE"] = str(
+            config(
+                "DJANGO_SETTINGS_MODULE",
+                default="bagitobjecttransfer.settings.test",
+            )
         )
 
     try:
