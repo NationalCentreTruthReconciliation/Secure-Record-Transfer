e190793209671cac7d1ad0d807083f7c177e379b
Daniel Lovegrove <d.lovegrove11@gmail.com>
2024-10-17 16:22:14 -0500

    Format file, and add recordtransfer:media url

--- bagitobjecttransfer/recordtransfer/urls.py
+++ bagitobjecttransfer/recordtransfer/urls.py
@@ -1,65 +1,102 @@
+from django.conf import settings
 from django.urls import path, re_path
 from django.forms import formset_factory
 from django.contrib.auth.decorators import login_required
 
 from . import views
 from . import forms
-from . import settings
+from . import settings as recordtransfersettings
 
 # Set up transfer forms depending on whether file uploads are enabled or disabled
-if settings.FILE_UPLOAD_ENABLED:
+if recordtransfersettings.FILE_UPLOAD_ENABLED:
     _transfer_forms = [
-        ('acceptlegal', forms.AcceptLegal),
-        ('contactinfo', forms.ContactInfoForm),
-        ('sourceinfo', forms.SourceInfoForm),
-        ('recorddescription', forms.RecordDescriptionForm),
-        ('rights', formset_factory(forms.RightsForm, formset=forms.RightsFormSet, extra=1)),
-        ('otheridentifiers', formset_factory(forms.OtherIdentifiersForm, extra=1)),
-        ('grouptransfer', forms.GroupTransferForm),
-        ('uploadfiles', forms.UploadFilesForm),
+        ("acceptlegal", forms.AcceptLegal),
+        ("contactinfo", forms.ContactInfoForm),
+        ("sourceinfo", forms.SourceInfoForm),
+        ("recorddescription", forms.RecordDescriptionForm),
+        ("rights", formset_factory(forms.RightsForm, formset=forms.RightsFormSet, extra=1)),
+        ("otheridentifiers", formset_factory(forms.OtherIdentifiersForm, extra=1)),
+        ("grouptransfer", forms.GroupTransferForm),
+        ("uploadfiles", forms.UploadFilesForm),
     ]
 else:
     _transfer_forms = [
-        ('acceptlegal', forms.AcceptLegal),
-        ('contactinfo', forms.ContactInfoForm),
-        ('sourceinfo', forms.SourceInfoForm),
-        ('recorddescription', forms.ExtendedRecordDescriptionForm), # Different
-        ('rights', formset_factory(forms.RightsForm, formset=forms.RightsFormSet, extra=1)),
-        ('otheridentifiers', formset_factory(forms.OtherIdentifiersForm, extra=1)),
-        ('grouptransfer', forms.GroupTransferForm),
-        ('finalnotes', forms.FinalStepFormNoUpload), # Different
+        ("acceptlegal", forms.AcceptLegal),
+        ("contactinfo", forms.ContactInfoForm),
+        ("sourceinfo", forms.SourceInfoForm),
+        ("recorddescription", forms.ExtendedRecordDescriptionForm),  # Different
+        ("rights", formset_factory(forms.RightsForm, formset=forms.RightsFormSet, extra=1)),
+        ("otheridentifiers", formset_factory(forms.OtherIdentifiersForm, extra=1)),
+        ("grouptransfer", forms.GroupTransferForm),
+        ("finalnotes", forms.FinalStepFormNoUpload),  # Different
     ]
 
-app_name = 'recordtransfer'
+app_name = "recordtransfer"
 urlpatterns = [
-    path('', views.Index.as_view(), name='index'),
-
-    path('transfer/', login_required(views.TransferFormWizard.as_view(_transfer_forms)), name='transfer'),
-    path('transfer/error/', login_required(views.SystemErrorPage.as_view()), name="systemerror"),
-    path('transfer/sent/', views.TransferSent.as_view(), name='transfersent'),
-    path('transfer/delete/<int:transfer_id>/', login_required(views.DeleteTransfer.as_view()), name="transferdelete"),
-
-    path('about/', views.About.as_view(), name='about'),
-    path('profile/', login_required(views.UserProfile.as_view()), name='userprofile'),
-    path('submission/<uuid:uuid>/', login_required(views.SubmissionDetail.as_view()), name="submissiondetail"),
-    path('submission/<uuid:uuid>/csv', login_required(views.SubmissionCsv.as_view()), name="submissioncsv"),
+    path("", views.Index.as_view(), name="index"),
+    path(
+        "transfer/",
+        login_required(views.TransferFormWizard.as_view(_transfer_forms)),
+        name="transfer",
+    ),
+    path("transfer/error/", login_required(views.SystemErrorPage.as_view()), name="systemerror"),
+    path("transfer/sent/", views.TransferSent.as_view(), name="transfersent"),
+    path(
+        "transfer/delete/<int:transfer_id>/",
+        login_required(views.DeleteTransfer.as_view()),
+        name="transferdelete",
+    ),
+    path("about/", views.About.as_view(), name="about"),
+    path("profile/", login_required(views.UserProfile.as_view()), name="userprofile"),
+    path(
+        "submission/<uuid:uuid>/",
+        login_required(views.SubmissionDetail.as_view()),
+        name="submissiondetail",
+    ),
+    path(
+        "submission/<uuid:uuid>/csv",
+        login_required(views.SubmissionCsv.as_view()),
+        name="submissioncsv",
+    ),
 ]
 
-if settings.FILE_UPLOAD_ENABLED:
-    urlpatterns.extend([
-        path('transfer/checkfile/', login_required(views.accept_file), name='checkfile'),
-        path('transfer/uploadfile/', login_required(views.uploadfiles), name='uploadfile'),
-    ])
+if settings.TESTING or not settings.DEBUG:
+    urlpatterns.append(
+        re_path(r"media/(?P<path>.*)", login_required(views.media_request), name="media")
+    )
 
-if settings.SIGN_UP_ENABLED:
-    urlpatterns.extend([
-        path('createaccount/', views.CreateAccount.as_view(), name='createaccount'),
-        path('createaccount/sent/', views.ActivationSent.as_view(), name='activationsent'),
-        path('createaccount/complete/', views.ActivationComplete.as_view(), name='accountcreated'),
-        path('createaccount/invalid/', views.ActivationInvalid.as_view(), name='activationinvalid'),
-        re_path(('createaccount/'
-                'activate/'
-                r'(?P<uidb64>[0-9A-Za-z_\-]+)/'
-                r'(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$'),
-            views.activate_account, name='activateaccount')
-    ])
+if recordtransfersettings.FILE_UPLOAD_ENABLED:
+    urlpatterns.extend(
+        [
+            path("transfer/checkfile/", login_required(views.accept_file), name="checkfile"),
+            path("transfer/uploadfile/", login_required(views.uploadfiles), name="uploadfile"),
+        ]
+    )
+
+if recordtransfersettings.SIGN_UP_ENABLED:
+    urlpatterns.extend(
+        [
+            path("createaccount/", views.CreateAccount.as_view(), name="createaccount"),
+            path("createaccount/sent/", views.ActivationSent.as_view(), name="activationsent"),
+            path(
+                "createaccount/complete/",
+                views.ActivationComplete.as_view(),
+                name="accountcreated",
+            ),
+            path(
+                "createaccount/invalid/",
+                views.ActivationInvalid.as_view(),
+                name="activationinvalid",
+            ),
+            re_path(
+                (
+                    "createaccount/"
+                    "activate/"
+                    r"(?P<uidb64>[0-9A-Za-z_\-]+)/"
+                    r"(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$"
+                ),
+                views.activate_account,
+                name="activateaccount",
+            ),
+        ]
+    )
