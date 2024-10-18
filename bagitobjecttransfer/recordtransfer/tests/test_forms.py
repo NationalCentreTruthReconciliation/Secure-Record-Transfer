e807131d602864e2f0af7955ff85940af42dedfa
Kelvin Oo <kelvinoo123@gmail.com>
2024-10-18 13:34:34 -0500

    Add more test cases for UserProfileForm

--- bagitobjecttransfer/recordtransfer/tests/test_forms.py
+++ bagitobjecttransfer/recordtransfer/tests/test_forms.py
@@ -27,6 +27,7 @@ class UserProfileFormTest(TestCase):
 
     def test_form_invalid_current_password(self):
         form_data = {
+            "gets_notification_emails": True,
             "current_password": "wrong_password",
             "new_password": "new_password123",
             "confirm_new_password": "new_password123",
@@ -35,7 +36,7 @@ class UserProfileFormTest(TestCase):
         self.assertFalse(form.is_valid())
         self.assertIn("current_password", form.errors)
 
-    def test_form_passwords_do_not_match(self):
+    def test_form_new_passwords_do_not_match(self):
         form_data = {
             "current_password": "old_password",
             "new_password": "new_password123",
@@ -45,9 +46,47 @@ class UserProfileFormTest(TestCase):
         self.assertFalse(form.is_valid())
         self.assertIn("confirm_new_password", form.errors)
 
+    def test_form_missing_current_password(self):
+        form_data = {
+            "new_password": "new_password123",
+            "confirm_new_password": "new_password123",
+        }
+        form = UserProfileForm(data=form_data, instance=self.user)
+        self.assertFalse(form.is_valid())
+        self.assertIn("current_password", form.errors)
+
+    def test_form_missing_new_password(self):
+        form_data = {
+            "current_password": "old_password",
+            "confirm_new_password": "new_password123",
+        }
+        form = UserProfileForm(data=form_data, instance=self.user)
+        self.assertFalse(form.is_valid())
+        self.assertIn("new_password", form.errors)
+
+    def test_form_missing_confirm_new_password(self):
+        form_data = {
+            "current_password": "old_password",
+            "new_password": "new_password123",
+        }
+        form = UserProfileForm(data=form_data, instance=self.user)
+        self.assertFalse(form.is_valid())
+        self.assertIn("confirm_new_password", form.errors)
+
+    def test_form_new_password_is_same_as_old_password(self):
+        form_data = {
+            "current_password": "old_password",
+            "new_password": "old_password",
+            "confirm_new_password": "old_password",
+        }
+        form = UserProfileForm(data=form_data, instance=self.user)
+        self.assertFalse(form.is_valid())
+        self.assertIn("new_password", form.errors)
+
     def test_form_empty(self):
         form_data = {}
         form = UserProfileForm(data=form_data, instance=self.user)
+        self.assertFalse(form.is_valid())
         self.assertIn("Form is empty.", form.errors["__all__"])
 
     def test_form_email_notification_initial_false(self):
@@ -70,3 +109,14 @@ class UserProfileFormTest(TestCase):
         self.assertTrue(form.is_valid())
         user = form.save()
         self.assertFalse(user.gets_notification_emails)
+
+    def test_form_no_changes(self):
+        form_data = {
+            "gets_notification_emails": True,
+            "current_password": "",
+            "new_password": "",
+            "confirm_new_password": "",
+        }
+        form = UserProfileForm(data=form_data, instance=self.user)
+        self.assertFalse(form.is_valid())
+        self.assertIn("No fields have been changed.", form.errors["__all__"])
