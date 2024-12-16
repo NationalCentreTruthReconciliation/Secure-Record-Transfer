5a7eafa738823b9502e022f623cbd09c1f661431
Kelvin Oo <kelvinoo123@gmail.com>
2024-12-16 12:48:50 -0600

    Refactor functions to arrow functions and clean code

--- bagitobjecttransfer/recordtransfer/static/recordtransfer/js/profile/profile.js
+++ bagitobjecttransfer/recordtransfer/static/recordtransfer/js/profile/profile.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
     const firstName = document.getElementById(ID_FIRST_NAME);
     const lastName = document.getElementById(ID_LAST_NAME);
     const getsNotificationEmails = document.getElementById(ID_GETS_NOTIFICATION_EMAILS);
@@ -23,34 +23,30 @@ document.addEventListener("DOMContentLoaded", function () {
         getsNotificationEmails: getsNotificationEmails.checked,
     };
 
-    function checkForChanges() {
-        const firstNameChanged = firstName.value !== initialValues.firstName;
-        const lastNameChanged = lastName.value !== initialValues.lastName;
-        const notificationChanged = getsNotificationEmails.checked !== initialValues.getsNotificationEmails;
+    const checkForChanges = () => {
+        const hasChanged = firstName.value !== initialValues.firstName ||
+            lastName.value !== initialValues.lastName ||
+            getsNotificationEmails.checked !== initialValues.getsNotificationEmails ||
+            (currentPassword.value && newPassword.value && confirmNewPassword.value);
 
-        const passwordFieldsPopulated = currentPassword.value !== "" &&
-            newPassword.value !== "" &&
-            confirmNewPassword.value !== "";
-
-        const hasChanged = firstNameChanged || lastNameChanged || notificationChanged || passwordFieldsPopulated;
         saveButton.disabled = !hasChanged;
-    }
+    };
 
-    Array.from(inputFields).forEach(input => {
+    inputFields.forEach(input => {
         input.addEventListener(input.type === "checkbox" ? "change" : "input", checkForChanges);
     });
 
     checkForChanges();
 
     // Save scroll position before page unload
-    window.addEventListener('beforeunload', function () {
+    window.addEventListener('beforeunload', () => {
         localStorage.setItem('scrollPosition', window.scrollY);
     });
 
     // Restore scroll position after page load
-    window.addEventListener('load', function () {
+    window.addEventListener('load', () => {
         const scrollPosition = localStorage.getItem('scrollPosition');
-        if (scrollPosition !== null) {
+        if (scrollPosition) {
             window.scrollTo(0, parseInt(scrollPosition, 10));
             localStorage.removeItem('scrollPosition');
         }
