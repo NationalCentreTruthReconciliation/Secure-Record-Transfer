27b30afb45bd86ab168c082d155cd710463ef893
Kelvin Oo <kelvinoo123@gmail.com>
2024-12-19 16:21:23 -0600

    Add JS to close modal

--- bagitobjecttransfer/recordtransfer/static/recordtransfer/js/transferform/modal.js
+++ bagitobjecttransfer/recordtransfer/static/recordtransfer/js/transferform/modal.js
@@ -1,12 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
     const modal = document.getElementById('unsaved-transferform-modal');
     const saveButton = document.getElementById('modal-save-button');
+    const closeButton = document.querySelector('.close-modal-button');
+    const cancelButton = document.getElementById('unsaved-transferform-modal-cancel');
     
     saveButton.addEventListener('click', (event) => {
         event.preventDefault();
-        // Find and click the form's save button
+        // Save and submit the form using the form's save button
         document.getElementById('form-save-button').click();
-        // Close the modal
+        modal.close();
+    });
+
+    closeButton.addEventListener('click', () => {
+        console.log('close button clicked');
+        modal.close();
+    });
+
+    cancelButton.addEventListener('click', () => {
         modal.close();
     });
 });
