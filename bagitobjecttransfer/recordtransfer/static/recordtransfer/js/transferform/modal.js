70377306dffa98aa5d0f4cd2523ea7c795a64523
Kelvin Oo <kelvinoo123@gmail.com>
2024-12-19 16:50:26 -0600

    Add JS to display modal when any link on transferform page is clicked

--- bagitobjecttransfer/recordtransfer/static/recordtransfer/js/transferform/modal.js
+++ bagitobjecttransfer/recordtransfer/static/recordtransfer/js/transferform/modal.js
@@ -3,7 +3,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const saveButton = document.getElementById('modal-save-button');
     const closeButton = document.querySelector('.close-modal-button');
     const cancelButton = document.getElementById('unsaved-transferform-modal-cancel');
-    
+    const leaveButton = document.getElementById('unsaved-transferform-modal-leave');
+    let targetUrl = '';
+
+    // Add event listeners to all links on page to unhide modal if clicked
+    document.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            targetUrl = event.currentTarget.href;
+            modal.classList.remove('hidden');
+        });
+    });
+
     saveButton.addEventListener('click', (event) => {
         event.preventDefault();
         // Save and submit the form using the form's save button
@@ -17,5 +28,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     cancelButton.addEventListener('click', () => {
         modal.classList.add('hidden');
+    });
+
+    leaveButton.addEventListener('click', () => {
+        window.location.href = targetUrl;
     });
 });
