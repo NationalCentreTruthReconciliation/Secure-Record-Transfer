71b15ea69c59afca4fea42a0eba0d28aaf7236df
Kelvin Oo <kelvinoo123@gmail.com>
2024-12-19 11:01:52 -0600

    Fix quotation marks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import stylisticJs from '@stylistic/eslint-plugin-js'
+import stylisticJs from "@stylistic/eslint-plugin-js";
 
 
 /** @type {import('eslint').Linter.Config[]} */
