2ffdf3cb7cd1a60f946b44852db023cbf428b126
Kelvin Oo <kelvinoo123@gmail.com>
2024-12-19 11:00:10 -0600

    Add more style rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,8 @@ export default [
         rules: {
             "@stylistic/js/indent": ["error", 4],
             "@stylistic/js/max-len": ["error", { code: 99, tabWidth: 4, ignoreUrls: true }],
+            "@stylistic/js/semi": ["error", "always"],
+            "@stylistic/js/quotes": ["error", "double"],
         }
     },
     pluginJs.configs.recommended,
