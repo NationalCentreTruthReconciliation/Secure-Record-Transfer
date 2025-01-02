c62464cee1791ec4363bf79f04c7fd8e7f65f116
Kelvin Oo <kelvinoo123@gmail.com>
2025-01-01 19:47:22 -0600

    Remove uppy style imports in JS

--- bagitobjecttransfer/recordtransfer/static/recordtransfer/js/transferform/uppyForm.js
+++ bagitobjecttransfer/recordtransfer/static/recordtransfer/js/transferform/uppyForm.js
@@ -1,9 +1,6 @@
 import Uppy from '@uppy/core';
 import DragDrop from '@uppy/drag-drop';
 
-import '@uppy/core/dist/style.min.css';
-import '@uppy/drag-drop/dist/style.min.css';
-
 document.addEventListener('DOMContentLoaded', () => {
     new Uppy().use(DragDrop, { target: "#drag-drop" });
 });
