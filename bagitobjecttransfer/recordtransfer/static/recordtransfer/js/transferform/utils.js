d95a98a101133c1f79ec93f599bc8929b2eed0c6
Kelvin Oo <kelvinoo123@gmail.com>
2025-01-06 09:03:33 -0600

    Format utils.js

--- bagitobjecttransfer/recordtransfer/static/recordtransfer/js/transferform/utils.js
+++ bagitobjecttransfer/recordtransfer/static/recordtransfer/js/transferform/utils.js
@@ -1,33 +1,32 @@
 /**
  * Get a cookie's value by its name.
- * @param {String} name The name of the cookie to retrieve from the user
- * @returns {String} The cookie value. null if it does not exist
+ * @param {string} name The name of the cookie to retrieve from the user
+ * @returns {string} The cookie value. null if it does not exist
  */
 export const getCookie = (name) => {
-    var cookieValue = null
-    if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';')
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+        var cookies = document.cookie.split(";");
         for (var i = 0; i < cookies.length; i++) {
-            var cookie = cookies[i].trim()
+            var cookie = cookies[i].trim();
             // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1))
-                break
+            if (cookie.substring(0, name.length + 1) === (name + "=")) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
             }
         }
     }
-    return cookieValue
-}
+    return cookieValue;
+};
 
 /**
  * Retrieves the upload session token from the DOM.
  * Looks for an input element with an ID ending in "session_token", which should exist on the
  * upload step of the transfer form.
- * 
  * @returns {?string} The value of the session token if found, null otherwise
  */
 export const getSessionToken = () => {
-    const sessionToken = document.querySelector('[id$="session_token"]')?.getAttribute("value");
+    const sessionToken = document.querySelector("[id$=\"session_token\"]")?.getAttribute("value");
     return sessionToken || null;
 };
 
@@ -35,11 +34,10 @@ export const getSessionToken = () => {
  * Sets the upload session token in the DOM.
  * Looks for an input element with an ID ending in "session_token", which should exist on the
  * upload step of the transfer form.
- * 
  * @param {string} token - The session token to set
  */
 export const setSessionToken = (token) => {
-    const sessionTokenElement = document.querySelector('[id$="session_token"]');
+    const sessionTokenElement = document.querySelector("[id$=\"session_token\"]");
     if (sessionTokenElement) {
         sessionTokenElement.setAttribute("value", token);
     }
@@ -47,7 +45,7 @@ export const setSessionToken = (token) => {
 
 /**
  * Retrieves file upload settings from context passed from view.
- * @returns {?Object} The parsed JSON settings object if element exists, null otherwise.
+ * @returns {?object} The parsed JSON settings object if element exists, null otherwise.
  */
 export const getSettings = () => {
     const settingsElement = document.getElementById("py_context_file_upload_settings");
@@ -55,26 +53,26 @@ export const getSettings = () => {
         return null;
     }
     return JSON.parse(settingsElement.textContent);
-}
+};
 
 export const fetchUploadedFiles = async () => {
     const sessionToken = getSessionToken();
     if (!sessionToken) {
-        console.error('Cannot fetch uploaded files without a session token');
-        return
+        console.error("Cannot fetch uploaded files without a session token");
+        return;
     }
     const response = await fetch(`/transfer/upload-session/${sessionToken}/files/`, {
-        method: 'GET',
+        method: "GET",
     });
     if (!response.ok) {
-        console.error('Failed to fetch uploaded files:', response.statusText);
+        console.error("Failed to fetch uploaded files:", response.statusText);
         return null;
     }
     return response.json();
-}
+};
 
 export const makeMockBlob = (size) => {
-    const blob = new Blob([''], { type: 'application/octet-stream' });
-    Object.defineProperty(blob, 'size', { value: size });
+    const blob = new Blob([""], { type: "application/octet-stream" });
+    Object.defineProperty(blob, "size", { value: size });
     return blob;
 };
