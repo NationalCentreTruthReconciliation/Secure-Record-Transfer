8af634e491acad5c2098755134eadca3e34c209f
Kelvin Oo <kelvinoo123@gmail.com>
2025-01-30 15:17:17 -0600

    Check config option instead of looking for e2e in file paths

--- conftest.py
+++ conftest.py
@@ -12,9 +12,8 @@ def pytest_collection_modifyitems(session, config, items) -> None:
     """Run npm build only if e2e tests are being run. `items` is a list of test items to be
     run.
     """
-    e2e_tests = any("e2e" in str(Path(item.fspath)) for item in items)
-
-    if e2e_tests:
+    # Check if running unit tests via -k parameter
+    if config.getoption("-k") == "e2e":
         try:
             subprocess.run(["npm", "run", "build"], check=True)
         except subprocess.CalledProcessError as e:
