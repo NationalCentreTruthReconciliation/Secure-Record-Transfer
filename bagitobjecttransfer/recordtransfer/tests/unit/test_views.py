c97c18c3a7e42ce684ff6cac53d72d3e74eacccf
Kelvin Oo <kelvinoo123@gmail.com>
2025-01-31 15:32:31 -0600

    Add tests for SubmissionGroupCreateView

--- bagitobjecttransfer/recordtransfer/tests/unit/test_views.py
+++ bagitobjecttransfer/recordtransfer/tests/unit/test_views.py
@@ -5,13 +5,14 @@ from unittest.mock import MagicMock, patch
 from django.contrib.messages import get_messages
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.forms import ValidationError
+from django.http import JsonResponse
 from django.test import TestCase
 from django.urls import reverse
 from django.utils.translation import gettext
 from override_storage import override_storage
 from override_storage.storage import LocMemStorage
 
-from recordtransfer.models import TempUploadedFile, UploadSession, User
+from recordtransfer.models import SubmissionGroup, TempUploadedFile, UploadSession, User
 
 
 class TestHomepage(TestCase):
@@ -305,6 +306,72 @@ class TestMediaRequestView(TestCase):
         self.assertTrue(response.has_header("X-Accel-Redirect"))
         self.assertFalse(response.has_header("Content-Type"))
         self.assertEqual(response.status_code, 200)
+
+
+class TestListUploadedFilesView(TestCase):
+    """Tests for recordtransfer:list_uploaded_files view."""
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """Set logging level."""
+        super().setUpClass()
+        logging.disable(logging.CRITICAL)
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        """Set up test data."""
+        cls.one_kib = bytearray([1] * 1024)
+        cls.test_user_1 = User.objects.create_user(username="testuser1", password="1X<ISRUkw+tuK")
+
+    def setUp(self) -> None:
+        """Set up test environment."""
+        _ = self.client.login(username="testuser1", password="1X<ISRUkw+tuK")
+        self.session = UploadSession.new_session(user=self.test_user_1)
+        self.url = reverse("recordtransfer:list_uploaded_files", args=[self.session.token])
+
+    def test_list_uploaded_files_session_not_found(self) -> None:
+        """Invalid session token."""
+        response = self.client.get(
+            reverse("recordtransfer:list_uploaded_files", args=["invalid_token"])
+        )
+        self.assertEqual(response.status_code, 404)
+        response_json = response.json()
+        self.assertIn("error", response_json)
+
+    def test_list_uploaded_files_invalid_user(self) -> None:
+        """Invalid user for the session."""
+        self.session.user = User.objects.create_user(
+            username="testuser2", password="1X<ISRUkw+tuK"
+        )
+        self.session.save()
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 404)
+        response_json = response.json()
+        self.assertIn("error", response_json)
+
+    def test_list_uploaded_files_empty_session(self) -> None:
+        """Session has no files."""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        response_json = response.json()
+        self.assertEqual(response_json.get("files"), [])
+
+    def test_list_uploaded_files_with_files(self) -> None:
+        """Session has one file."""
+        file_to_upload = SimpleUploadedFile("testfile.txt", self.one_kib)
+        temp_file = self.session.add_temp_file(file_to_upload)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        response_json = response.json()
+        responseFiles = response_json.get("files")
+        self.assertEqual(len(responseFiles), 1)
+        self.assertEqual(responseFiles[0]["name"], "testfile.txt")
+        self.assertEqual(responseFiles[0]["size"], file_to_upload.size)
+        self.assertEqual(responseFiles[0]["url"], temp_file.get_file_access_url())
+
+    def tearDown(self) -> None:
+        TempUploadedFile.objects.all().delete()
+        UploadSession.objects.all().delete()
 
 
 class TestUploadedFileView(TestCase):
@@ -704,3 +771,97 @@ class TestCreateUploadSessionView(TestCase):
         response_json = response.json()
         self.assertIn("error", response_json)
 
+
+
+class TestSubmissionGroupCreateView(TestCase):
+    """Tests for SubmissionGroupCreateView."""
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        """Set up test data."""
+        cls.user = User.objects.create_user(username="testuser", password="password")
+        cls.url = reverse("recordtransfer:submissiongroupnew")
+
+    def setUp(self) -> None:
+        """Set up test environment."""
+        self.client.login(username="testuser", password="password")
+
+    def test_access_authenticated_user(self) -> None:
+        """Test that an authenticated user can access the view."""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "recordtransfer/submission_group_show_create.html")
+
+    def test_access_unauthenticated_user(self) -> None:
+        """Test that an unauthenticated user is redirected to the login page."""
+        self.client.logout()
+        response = self.client.get(self.url)
+        self.assertRedirects(response, f"{reverse('login')}?next={self.url}")
+
+    def test_valid_form_submission(self) -> None:
+        """Test that a valid form submission creates a new SubmissionGroup."""
+        form_data = {
+            "name": "Test Group",
+            "description": "Test Description",
+        }
+        response = self.client.post(self.url, data=form_data)
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(str(messages[0]), gettext("Group created"))
+        self.assertTrue(SubmissionGroup.objects.filter(name="Test Group").exists())
+
+    def test_invalid_form_submission(self) -> None:
+        """Test that an invalid form submission does not create a new SubmissionGroup."""
+        form_data = {
+            "name": "",
+            "description": "Test Description",
+        }
+        response = self.client.post(self.url, data=form_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "recordtransfer/submission_group_show_create.html")
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(str(messages[0]), gettext("There was an error creating the group"))
+
+    @patch("recordtransfer.views.SubmissionGroupCreateView.form_valid")
+    def test_form_valid_json_response(self, mock_form_valid: MagicMock) -> None:
+        """Test that a JsonResponse is returned when the form is valid and submitted from the
+        transfer page.
+        """
+        mock_form_valid.return_value = JsonResponse(
+            {
+                "message": gettext("Group created"),
+                "status": "success",
+                "group": {
+                    "uuid": "test-uuid",
+                    "name": "Test Group",
+                    "description": "Test Description",
+                },
+            },
+            status=200,
+        )
+        form_data = {
+            "name": "Test Group",
+            "description": "Test Description",
+        }
+        response = self.client.post(self.url, data=form_data, HTTP_REFERER="transfer")
+        self.assertEqual(response.status_code, 200)
+        response_json = response.json()
+        self.assertEqual(response_json["message"], gettext("Group created"))
+        self.assertEqual(response_json["status"], "success")
+
+    @patch("recordtransfer.views.SubmissionGroupCreateView.form_invalid")
+    def test_form_invalid_json_response(self, mock_form_invalid: MagicMock) -> None:
+        """Test that a JsonResponse is returned when the form is invalid and submitted from the
+        transfer page.
+        """
+        mock_form_invalid.return_value = JsonResponse(
+            {"message": "This field is required.", "status": "error"}, status=400
+        )
+        form_data = {
+            "name": "",
+            "description": "Test Description",
+        }
+        response = self.client.post(self.url, data=form_data, HTTP_REFERER="transfer")
+        self.assertEqual(response.status_code, 400)
+        response_json = response.json()
+        self.assertEqual(response_json["message"], "This field is required.")
+        self.assertEqual(response_json["status"], "error")
