44f351e8ef35ba807fe802b3f2e75e1248869c4b
Daniel Lovegrove <d.lovegrove11@gmail.com>
2025-02-04 13:04:20 -0600

    Auto-format file

--- bagitobjecttransfer/recordtransfer/static/recordtransfer/js/admin/hideMediaLink.js
+++ bagitobjecttransfer/recordtransfer/static/recordtransfer/js/admin/hideMediaLink.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
     // Replace links for media URLs to prevent downloading files
-    document.querySelectorAll('a').forEach((link) => {
-        let href = link.getAttribute('href');
-        if (href.includes('media/')) {
-            link.setAttribute('href', '#');
-            link.addEventListener('click', (e) => {
+    document.querySelectorAll("a").forEach((link) => {
+        const href = link.getAttribute("href");
+        if (href.includes("media/")) {
+            link.setAttribute("href", "#");
+            link.addEventListener("click", (e) => {
                 e.preventDefault();
-                alert('Files can only be downloaded by creating a BagIt Bag for a submission');
+                alert("Files can only be downloaded by creating a BagIt Bag for a submission");
             });
         }
     });
