ad7bc0aee0293e07106fa1437b8369f02b9921e6
Kelvin Oo <kelvinoo123@gmail.com>
2025-02-24 09:42:07 -0600

    Add warning and return if domain begins with http or https

--- app/recordtransfer/management/commands/set_domain.py
+++ app/recordtransfer/management/commands/set_domain.py
@@ -7,7 +7,10 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     """Set the domain of the current site."""
 
+    INVALID_PREFIXES = ("http:", "https:")
+
     help = "Sets the domain of the current site"
+
     def add_arguments(self, parser: ArgumentParser) -> None:
         """Add arguments to the command."""
         parser.add_argument(
@@ -18,7 +21,7 @@ class Command(BaseCommand):
         parser.add_argument(
             "--display-name",
             type=str,
-            help='Optional display name to set for the current site',
+            help="Optional display name to set for the current site",
             default=None,
         )
 
@@ -26,6 +29,15 @@ class Command(BaseCommand):
         """Set the domain of the current site."""
         domain = options["domain"]
         display_name = options["display_name"]
+
+        if any(domain.startswith(prefix) for prefix in Command.INVALID_PREFIXES):
+            self.stdout.write(
+                self.style.WARNING(
+                    "Domain should not start with any of the following prefixes: "
+                    f"{', '.join(Command.INVALID_PREFIXES)}"
+                )
+            )
+            return
 
         # Get the current site
         current_site = Site.objects.get_current()
