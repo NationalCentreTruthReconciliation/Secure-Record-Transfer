b808c1c48fb68dd8669d864cccbb33fa6ee66a84
Kelvin Oo <kelvinoo123@gmail.com>
2025-02-24 13:29:15 -0600

    Check that desired functions are called instead

--- app/recordtransfer/tests/unit/test_signals.py
+++ app/recordtransfer/tests/unit/test_signals.py
@@ -1,5 +1,5 @@
 import logging
-import os
+from unittest.mock import MagicMock, patch
 
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
@@ -30,41 +30,54 @@ class TestUploadedFileSignal(TestCase):
             "test1.pdf", b"Test file content", content_type="application/pdf"
         )
 
-    def test_file_exists_on_temp_file_creation(self) -> None:
-        """Test that the file exists when a TempUploadedFile model instance is created."""
-        temp_uploaded_file = TempUploadedFile(
-            name="test.pdf", session=self.session, file_upload=self.test_file
-        )
-        temp_uploaded_file.save()
-        self.assertTrue(os.path.exists(temp_uploaded_file.file_upload.path))
-
-    def test_file_deleted_on_temp_file_delete(self) -> None:
+    @patch("django.core.files.storage.FileSystemStorage.delete")
+    def test_file_deleted_on_temp_file_delete(self, mock_delete: MagicMock) -> None:
         """Test that the file is deleted when the TempUploadedFile model instance is deleted."""
         temp_uploaded_file = TempUploadedFile(
             name="test.pdf", session=self.session, file_upload=self.test_file
         )
         temp_uploaded_file.save()
-        file_path = temp_uploaded_file.file_upload.path
         temp_uploaded_file.delete()
-        self.assertFalse(os.path.exists(file_path))
+        mock_delete.assert_called_once()
 
-    def test_file_exist_on_perm_file_creation(self) -> None:
-        """Test that the file exists when a PermUploadedFile model instance is created."""
-        perm_uploaded_file = PermUploadedFile(
-            name="test.pdf", session=self.session, file_upload=self.test_file
-        )
-        perm_uploaded_file.save()
-        self.assertTrue(os.path.exists(perm_uploaded_file.file_upload.path))
-
-    def test_file_deleted_on_perm_file_delete(self) -> None:
+    @patch("django.core.files.storage.FileSystemStorage.delete")
+    def test_file_deleted_on_perm_file_delete(self, mock_delete: MagicMock) -> None:
         """Test that the file is deleted when the PermUploadedFile model instance is deleted."""
         perm_uploaded_file = PermUploadedFile(
             name="test.pdf", session=self.session, file_upload=self.test_file
         )
         perm_uploaded_file.save()
-        file_path = perm_uploaded_file.file_upload.path
         perm_uploaded_file.delete()
-        self.assertFalse(os.path.exists(file_path))
+        mock_delete.assert_called_once()
+
+    @patch.object(UploadSession, "touch")
+    def test_upload_session_touch_called_on_temp_file_deletion(
+        self, mock_touch: MagicMock
+    ) -> None:
+        """Test that the upload session's touch method is called when a TempUploadedFile model
+        instance is deleted.
+        """
+        temp_uploaded_file = TempUploadedFile(
+            name="test.pdf", session=self.session, file_upload=self.test_file
+        )
+        temp_uploaded_file.save()
+        temp_uploaded_file.delete()
+        mock_touch.assert_called_once()
+
+    @patch.object(UploadSession, "touch")
+    def test_upload_session_touch_not_called_on_perm_file_deletion(
+        self, mock_touch: MagicMock
+    ) -> None:
+        """Test that the upload session's touch method is not called when a PermUploadedFile model
+        instance is deleted.
+        """
+        perm_uploaded_file = PermUploadedFile(
+            name="test.pdf", session=self.session, file_upload=self.test_file
+        )
+        perm_uploaded_file.save()
+        perm_uploaded_file.delete()
+        mock_touch.assert_not_called()
+
 
 class TestInProgressSubmissionSignal(TestCase):
     """Test the signals for the InProgressSubmission model."""
@@ -75,14 +88,14 @@ class TestInProgressSubmissionSignal(TestCase):
         super().setUpClass()
         logging.disable(logging.CRITICAL)
 
-    def test_touch_called_on_in_progress_submission_save(self) -> None:
+    @patch.object(UploadSession, "touch")
+    def test_touch_called_on_in_progress_submission_save(self, mock_touch: MagicMock) -> None:
         """Test that the upload session's touch method is called when an InProgressSubmission
-        model instance is saved.
+        instance is saved.
         """
-        # Create initial upload session and store its last interaction time
+        # Create upload session
         user = User.objects.create(username="testuser", password="password")
         upload_session = UploadSession.new_session(user=user)
-        initial_interaction_time = upload_session.last_upload_interaction_time
 
         # Create and save an InProgressSubmission with the upload session
         in_progress_submission = InProgressSubmission(
@@ -92,11 +105,5 @@ class TestInProgressSubmissionSignal(TestCase):
         )
         in_progress_submission.save()
 
-        # Get the updated upload session from database
-        upload_session.refresh_from_db()
-
-        # Verify the last interaction time was updated
-        self.assertGreater(
-            upload_session.last_upload_interaction_time,
-            initial_interaction_time
-        )
+        # Verify touch() was called once
+        mock_touch.assert_called_once()
