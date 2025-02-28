ec1703f495361b0ab9b5316b7f565074e6814bbb
Kelvin Oo <kelvinoo123@gmail.com>
2025-02-28 14:55:20 -0600

    Set django as a global

--- app/caais/static/caais/js/admin/phoneNumberMask.js
+++ app/caais/static/caais/js/admin/phoneNumberMask.js
@@ -1,8 +1,9 @@
+/* global django */
+
 window.addEventListener("load", function() {
     (function($) {
         $("input[id$=\"-phone_number\"").each(function() {
             $(this).mask("+0 (000) 000-0000");
         });
-    // eslint-disable-next-line no-undef
     })(django.jQuery);
 });
