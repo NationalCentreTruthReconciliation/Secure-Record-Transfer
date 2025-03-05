7db9a7ab68af897a4bb08e77a02cb4e563771841
Kelvin Oo <kelvinoo123@gmail.com>
2025-03-05 13:45:23 -0600

    Update test file for testing TransferFormWizard

--- app/recordtransfer/tests/unit/views/test_transfer.py
+++ app/recordtransfer/tests/unit/views/test_transfer.py
@@ -1,11 +1,13 @@
 from typing import cast
-from unittest.mock import MagicMock, patch
+from unittest.mock import PropertyMock, patch
 
+from caais.models import RightsType, SourceRole, SourceType
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import RequestFactory, TestCase
 from django.urls import reverse
 
-from recordtransfer.models import InProgressSubmission, UploadSession, User
-from recordtransfer.views.transfer import TransferFormWizard
+from recordtransfer.enums import TransferStep
+from recordtransfer.models import InProgressSubmission, SubmissionGroup, UploadSession, User
 
 
 class TransferFormWizardTests(TestCase):
@@ -15,32 +17,206 @@ class TransferFormWizardTests(TestCase):
         """Set up the test case with a user and an in-progress submission."""
         self.factory = RequestFactory()
         self.user = User.objects.create_user(
-            username="testuser", email="test@example.com", password="testpassword"
+            username="testuser1", email="test@example.com", password="testpassword"
         )
-        self.upload_session = UploadSession.new_session(user=cast(User, self.user))
-        self.in_progress_submission = InProgressSubmission.objects.create(
-            user=self.user,
-            uuid="550e8400-e29b-41d4-a716-446655440000",
-            upload_session=self.upload_session,
-        )
+        self.client.force_login(self.user)
+        self.url = reverse("recordtransfer:transfer")
 
-    @patch("recordtransfer.models.UploadSession.is_expired", new_callable=MagicMock)
-    @patch("recordtransfer.views.transfer.TransferFormWizard.get")
-    def test_resume_in_progress_submission_redirects_to_submission_expired(
-        self, mock_get: MagicMock, mock_expired: MagicMock
-    ) -> None:
+        self.test_data = [
+            (TransferStep.ACCEPT_LEGAL.value, {"agreement_accepted": "on"}),
+            (
+                TransferStep.CONTACT_INFO.value,
+                {
+                    "contact_name": "John Doe",
+                    "phone_number": "+1 (999) 999-9999",
+                    "email": "john.doe@example.com",
+                    "address_line_1": "123 Main St",
+                    "city": "Winnipeg",
+                    "province_or_state": "MB",
+                    "postal_or_zip_code": "R3C 1A5",
+                    "country": "CA",
+                    "job_title": "Archivist",
+                    "organization": "Test Organization",
+                },
+            ),
+            (
+                TransferStep.SOURCE_INFO.value,
+                {
+                    "enter_manual_source_info": "yes",
+                    "source_name": "Test Source Name",
+                    "source_type": SourceType.objects.filter(name="Individual").first().pk,
+                    "source_role": SourceRole.objects.filter(name="Donor").first().pk,
+                    "source_note": "Test Source Note",
+                    "preliminary_custodial_history": "Test Custodial History",
+                },
+            ),
+            (
+                TransferStep.RECORD_DESCRIPTION.value,
+                {
+                    "accession_title": "Test Accession Title",
+                    "date_of_materials": "2021-01-01 - 2021-12-31",
+                    "language_of_material": "English",
+                    "preliminary_scope_and_content": "Test Description",
+                    "condition": "Test Condition",
+                },
+            ),
+            (
+                TransferStep.RIGHTS.value,
+                [
+                    {
+                        "rights_type": RightsType.objects.filter(name="Copyright").first().pk,
+                        "rights_value": "Test Note for Rights",
+                    },
+                ],
+            ),
+            (
+                TransferStep.OTHER_IDENTIFIERS.value,
+                [
+                    {
+                        "other_identifier_type": "Test Identifier Type",
+                        "other_identifier_value": "Test Identifier Value",
+                        "other_identifier_note": "Test Identifier Note",
+                    },
+                ],
+            ),
+            (
+                TransferStep.GROUP_TRANSFER.value,
+                {
+                    "group_uuid": SubmissionGroup.objects.create(
+                        name="Test Group", created_by=self.user
+                    ).uuid,
+                },
+            ),
+            (
+                TransferStep.UPLOAD_FILES.value,
+                {
+                    "general_note": "Test General Note",
+                },
+            ),
+            (
+                TransferStep.REVIEW.value,
+                {},
+            ),
+        ]
+
+    def _upload_test_file(self) -> str:
+        """Upload a test file to the server. Returns the upload session token used to upload the
+        file.
+        """
+        response = self.client.post(reverse("recordtransfer:create_upload_session"))
+        self.assertEqual(201, response.status_code)
+        session_token = response.json()["uploadSessionToken"]
+
+        test_file = SimpleUploadedFile("test_file.txt", b"file_content", content_type="text/plain")
+        response = self.client.post(
+            reverse("recordtransfer:upload_files", kwargs={"session_token": session_token}),
+            {"file": test_file},
+        )
+        self.assertEqual(200, response.status_code)
+
+        return session_token
+
+    def _process_test_data(self, step: str, step_data: dict) -> dict:
+        """Process the test data for the given step for form submission."""
+        submit_data = {"transfer_form_wizard-current_step": step}
+
+        if step == TransferStep.UPLOAD_FILES.value:
+            session_token = self._upload_test_file()
+            submit_data[f"{step}-session_token"] = session_token
+
+        if type(step_data) is dict:
+            submit_data.update(
+                {"{}-{}".format(step, key): value for key, value in step_data.items()}
+            )
+        elif type(step_data) is list:
+            submit_data["{}-TOTAL_FORMS".format(step)] = str(len(step_data))
+            submit_data["{}-INITIAL_FORMS".format(step)] = str(len(step_data))
+            for i, item in enumerate(step_data):
+                submit_data.update(
+                    {"{}-{}-{}".format(step, i, key): value for key, value in item.items()}
+                )
+
+        return submit_data
+
+    def test_wizard(self) -> None:
+        """Test the TransferFormWizard view from start to finish. This test will fill out the form
+        with the test data and submit it, making sure no errors are raised.
+        """
+        self.assertEqual(200, self.client.get(self.url).status_code)
+        for step, step_data in self.test_data:
+            submit_data = self._process_test_data(step, step_data)
+
+            response = self.client.post(self.url, submit_data, follow=True)
+            self.assertEqual(200, response.status_code)
+
+            if step != TransferStep.REVIEW.value and "form" in response.context:
+                self.assertFalse(response.context["form"].errors)
+
+    @patch("django.conf.settings.UPLOAD_SESSION_EXPIRE_AFTER_INACTIVE_MINUTES", 60)
+    def test_saving_expirable_in_progress_submission(self) -> None:
+        """Test that saving an expirable in-progress submission. Saves the form on the
+        Upload Files step after uploading a file.
+        """
+        self.assertEqual(200, self.client.get(self.url).status_code)
+        # Check that the in-progress submission does not exist yet
+        self.assertFalse(self.user.inprogresssubmission_set.exists())
+
+        for step, step_data in self.test_data:
+            submit_data = self._process_test_data(step, step_data)
+            if step == TransferStep.UPLOAD_FILES.value:
+                submit_data["save_form_step"] = step
+                response = self.client.post(self.url, submit_data, follow=True)
+                self.assertEqual(200, response.status_code)
+                self.assertRedirects(response, reverse("recordtransfer:userprofile"))
+                self.assertContains(
+                    response, "Transfer saved successfully. This submission will expire on"
+                )
+                self.assertTrue(self.user.inprogresssubmission_set.exists())
+                self.assertFalse(self.user.inprogresssubmission_set.first().upload_session_expired)
+                break
+            else:
+                response = self.client.post(self.url, submit_data, follow=True)
+                self.assertEqual(200, response.status_code)
+
+    @patch("django.conf.settings.UPLOAD_SESSION_EXPIRE_AFTER_INACTIVE_MINUTES", 60)
+    def test_saving_unexpirable_in_progress_submission(self) -> None:
+        """Test saving an unexpirable in-progress submission. Saves the form on the Rights step
+        after filling out the form.
+        """
+        self.assertEqual(200, self.client.get(self.url).status_code)
+        # Check that the in-progress submission does not exist yet
+        self.assertFalse(self.user.inprogresssubmission_set.exists())
+
+        for step, step_data in self.test_data:
+            submit_data = self._process_test_data(step, step_data)
+            if step == TransferStep.RIGHTS.value:
+                submit_data["save_form_step"] = step
+                response = self.client.post(self.url, submit_data, follow=True)
+                self.assertEqual(200, response.status_code)
+                self.assertRedirects(response, reverse("recordtransfer:userprofile"))
+                self.assertContains(response, "Transfer saved successfully.")
+                self.assertNotContains(response, "This submission will expire on")
+                self.assertTrue(self.user.inprogresssubmission_set.exists())
+                self.assertFalse(self.user.inprogresssubmission_set.first().upload_session_expired)
+                break
+            else:
+                response = self.client.post(self.url, submit_data, follow=True)
+                self.assertEqual(200, response.status_code)
+
+    @patch(
+        "recordtransfer.models.InProgressSubmission.upload_session_expired",
+        new_callable=PropertyMock,
+    )
+    def test_resume_expired_in_progress_submission(self, mock_expired: PropertyMock) -> None:
         """Test that the view redirects to the submission expired page if the in-progress
         submission is expired.
         """
-        mock_expired.return_value = True
-
-        request = self.factory.get(
-            reverse("recordtransfer:transfer"),
-            {"transfer_uuid": "550e8400-e29b-41d4-a716-446655440000"},
+        upload_session = UploadSession.new_session(user=cast(User, self.user))
+        in_progress = InProgressSubmission.objects.create(
+            user=self.user,
+            upload_session=upload_session,
+            current_step=TransferStep.CONTACT_INFO.value,
         )
-        request.user = self.user
-
-        response = TransferFormWizard.as_view()(request)
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, reverse("recordtransfer:in_progress_submission_expired"))
+        mock_expired.return_value = True
+        response = self.client.get(self.url, {"transfer_uuid": in_progress.uuid}, follow=True)
+        self.assertRedirects(response, reverse("recordtransfer:in_progress_submission_expired"))
