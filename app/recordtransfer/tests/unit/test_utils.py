ca91e85b66e750548cce807e294cc444f0ba59ce
Eashana Singhal <106555593+Eashana0104@users.noreply.github.com>
2025-05-08 11:03:54 -0500

    540 Remove optional logger arguments (#552)
    * Refactor models.py to use module loggers only
    
    * Refactor utils.py to use module loggers only
    
    * Fix unit tests for utils.py caused by refactoring utils.py
    
    * Fix more unit tests by making change in models.py
    
    * Remove redundant assignment to logger
    
    * Remove redundant assignment to logger

--- app/recordtransfer/tests/unit/test_utils.py
+++ app/recordtransfer/tests/unit/test_utils.py
@@ -148,28 +148,22 @@ class FileCountingUtilityTests(TestCase):
         cls.null_logger = NullLogger()
 
     def test_no_files_counted(self):
-        counted_types = count_file_types([], self.accepted_formats, logger=self.null_logger)
+        counted_types = count_file_types([], self.accepted_formats)
         self.assertEqual(counted_types, {})
 
     def test_no_accepted_file_types(self):
         counted_types = count_file_types(
-            ["file1.pages", "file2.docs", "file3.pdf"],
-            self.accepted_formats,
-            logger=self.null_logger,
+            ["file1.pages", "file2.docs", "file3.pdf"], self.accepted_formats
         )
         self.assertEqual(counted_types, {})
 
     def test_one_file_counted(self):
-        counted_types = count_file_types(
-            ["song1.mp3"], self.accepted_formats, logger=self.null_logger
-        )
+        counted_types = count_file_types(["song1.mp3"], self.accepted_formats)
         self.assertEqual(counted_types, {"Audio": 1})
 
     def test_multiple_of_one_file_type_counted(self):
         counted_types = count_file_types(
-            ["file1.doc", "file2.doc", "file3.docx"],
-            self.accepted_formats,
-            logger=self.null_logger,
+            ["file1.doc", "file2.doc", "file3.docx"], self.accepted_formats
         )
         self.assertEqual(counted_types, {"Microsoft Word Document": 3})
 
@@ -177,7 +171,6 @@ class FileCountingUtilityTests(TestCase):
         counted_types = count_file_types(
             ["file1.doc", "file2.doc", "song1.mp3", "song1.acc", "sheet1.xls", "sheet2.xlsx"],
             self.accepted_formats,
-            logger=self.null_logger,
         )
         self.assertEqual(
             counted_types,
@@ -190,45 +183,33 @@ class FileCountingUtilityTests(TestCase):
 
     def test_uppercase_extensions_are_counted(self):
         counted_types = count_file_types(
-            ["file1.DOC", "file2.doc", "file3.docx", "file4.DOCX"],
-            self.accepted_formats,
-            logger=self.null_logger,
+            ["file1.DOC", "file2.doc", "file3.docx", "file4.DOCX"], self.accepted_formats
         )
         self.assertEqual(counted_types, {"Microsoft Word Document": 4})
 
     def test_unaccepted_files_ignored(self):
         counted_types = count_file_types(
-            ["file1.pages", "file2.docs", "file3.pdf", "file4.docx"],
-            self.accepted_formats,
-            logger=self.null_logger,
+            ["file1.pages", "file2.docs", "file3.pdf", "file4.docx"], self.accepted_formats
         )
         self.assertEqual(counted_types, {"Microsoft Word Document": 1})
 
     def test_no_files_human_readable(self):
-        statement = get_human_readable_file_count(
-            [], self.accepted_formats, logger=self.null_logger
-        )
+        statement = get_human_readable_file_count([], self.accepted_formats)
         self.assertEqual(statement, "No file types could be identified")
 
     def test_no_accepted_file_types_human_readable(self):
         statement = get_human_readable_file_count(
-            ["file1.pages", "file2.docs", "file3.pdf"],
-            self.accepted_formats,
-            logger=self.null_logger,
+            ["file1.pages", "file2.docs", "file3.pdf"], self.accepted_formats
         )
         self.assertEqual(statement, "No file types could be identified")
 
     def test_one_file_group_singular_human_readable(self):
-        statement = get_human_readable_file_count(
-            ["file1.DOC"], self.accepted_formats, logger=self.null_logger
-        )
+        statement = get_human_readable_file_count(["file1.DOC"], self.accepted_formats)
         self.assertEqual(statement, "1 Microsoft Word Document file")
 
     def test_one_file_group_plural_human_readable(self):
         statement = get_human_readable_file_count(
-            ["file1.DOC", "file2.doc", "file3.docx", "file4.DOCX"],
-            self.accepted_formats,
-            logger=self.null_logger,
+            ["file1.DOC", "file2.doc", "file3.docx", "file4.DOCX"], self.accepted_formats
         )
         self.assertEqual(statement, "4 Microsoft Word Document files")
 
@@ -236,7 +217,6 @@ class FileCountingUtilityTests(TestCase):
         statement = get_human_readable_file_count(
             ["file1.DOC", "file2.doc", "song1.mp3", "img1.jpg", "img2.png", "img3.png"],
             self.accepted_formats,
-            logger=self.null_logger,
         )
         self.assertIn("2 Microsoft Word Document files", statement)
         self.assertIn("1 Audio file", statement)
