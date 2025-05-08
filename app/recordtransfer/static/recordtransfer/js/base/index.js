33224fcd4dfbae0c523a69512575bf26c5fd43c0
Kelvin Oo <kelvinoo123@gmail.com>
2025-05-08 16:39:11 -0500

    Manage htmx dependency ourselves

--- app/recordtransfer/static/recordtransfer/js/base/index.js
+++ app/recordtransfer/static/recordtransfer/js/base/index.js
@@ -1,7 +1,9 @@
 import "htmx-ext-head-support";
+import htmx from "htmx.org";
 import { setupMessages } from "./messages";
 import { setupNavbar } from "./navbar";
 
+window.htmx = htmx;
 document.addEventListener("DOMContentLoaded", () => {
     setupNavbar();
     setupMessages();
