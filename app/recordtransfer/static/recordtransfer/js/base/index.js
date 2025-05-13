fcf995150555696d156752b3e5a0dc7b1c633280
Eashana <eashanasinghal@gmail.com>
2025-05-13 08:45:00 -0500

    Add htmx back to index.js

--- app/recordtransfer/static/recordtransfer/js/base/index.js
+++ app/recordtransfer/static/recordtransfer/js/base/index.js
@@ -1,8 +1,10 @@
+import "htmx-ext-head-support";
+import htmx from "htmx.org";
 import { setupMessages } from "./messages";
 import { setupNavbar } from "./navbar";
 import { setupHelpTooltips } from "./tooltip";
 
-
+window.htmx = htmx;
 document.addEventListener("DOMContentLoaded", () => {
     setupNavbar();
     setupMessages();
