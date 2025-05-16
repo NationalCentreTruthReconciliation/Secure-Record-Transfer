04bcbb0659727016ba0bcd53252454386909c281
Kelvin Oo <kelvinoo123@gmail.com>
2025-05-16 12:36:30 -0500

    Remove JS for deleting submission group

--- app/recordtransfer/static/recordtransfer/js/submission_group/submission_group_form.js
+++ app/recordtransfer/static/recordtransfer/js/submission_group/submission_group_form.js
@@ -1,5 +1,3 @@
-import { setupConfirmModal } from "../confirm_modal/confirm_modal";
-
 document.addEventListener("DOMContentLoaded", function () {
     const contextElement = document.getElementById("py_context_submission_group");
 
@@ -37,24 +35,6 @@ document.addEventListener("DOMContentLoaded", function () {
     Array.from(inputFields).forEach(input => {
         input.addEventListener("input", checkForChanges);
     });
-
-    const deleteButton = document.getElementById("id_delete_group_button");
-    if (deleteButton) {
-        const handleDeleteConfirm = function() {
-            fetch(context["DELETE_URL"], {
-                method: "DELETE",
-                headers: {
-                    "X-CSRFToken": document.querySelector("[name=csrfmiddlewaretoken]").value
-                }
-            }).then(response => {
-                if (response.redirected) {
-                    window.location.href = response.url;
-                }
-            });
-        };
-        
-        setupConfirmModal(deleteButton, handleDeleteConfirm);
-    }
 
     checkForChanges();
 });
