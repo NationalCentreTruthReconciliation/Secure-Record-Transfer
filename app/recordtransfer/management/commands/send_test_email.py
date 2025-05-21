1efaad0844afd856ee1dfa7b561bbf1573909c98
Eashana <eashanasinghal@gmail.com>
2025-05-21 12:28:51 -0500

    Add missing list brackets

--- app/recordtransfer/management/commands/send_test_email.py
+++ app/recordtransfer/management/commands/send_test_email.py
@@ -140,12 +140,15 @@ class Command(BaseCommand):
         elif email_id == "user_activation_email":
             func(user)
         elif email_id == "user_account_updated":
-            func(user, {
-                "subject": "Account updated",
-                "changed_item": "account",
-                "changed_status": "updated",
-                "changed_list": "Staff privileges have been added to your account.",
-            })
+            func(
+                user,
+                {
+                    "subject": "Account updated",
+                    "changed_item": "account",
+                    "changed_status": "updated",
+                    "changed_list": ["Staff privileges have been added to your account."],
+                },
+            )
         elif email_id == "in_progress_submission_expiring":
             func(in_progress)
         else:
