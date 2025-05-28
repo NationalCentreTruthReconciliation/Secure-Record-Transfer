101b3fef8ec0a5cfca9a30fece505755153493cc
Kelvin Oo <kelvinoo123@gmail.com>
2025-05-28 16:00:03 -0500

    Removed irrelevant tests from SubmissionGroupDetailView

--- app/recordtransfer/tests/unit/views/test_post_submission.py
+++ app/recordtransfer/tests/unit/views/test_post_submission.py
@@ -140,10 +140,8 @@ class TestSubmissionGroupDetailView(TestCase):
             "description": "Updated Description",
         }
         response = self.client.post(self.group_detail_url, data=form_data)
-        # Check that the user is redirected back to the same page
-        self.assertRedirects(response, self.group_detail_url)
-        messages = list(get_messages(response.wsgi_request))
-        self.assertEqual(str(messages[0]), gettext("Group updated"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "recordtransfer/submission_group_detail.html")
         self.group.refresh_from_db()
         self.assertEqual(self.group.name, "Updated Group")
         self.assertEqual(self.group.description, "Updated Description")
@@ -157,45 +155,8 @@ class TestSubmissionGroupDetailView(TestCase):
         response = self.client.post(self.group_detail_url, data=form_data)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "recordtransfer/submission_group_detail.html")
-        messages = list(get_messages(response.wsgi_request))
-        self.assertEqual(str(messages[0]), gettext("There was an error updating the group"))
         self.group.refresh_from_db()
         self.assertNotEqual(self.group.description, "Updated Description")
-
-    def test_get_context_data(self) -> None:
-        """Test that the context data includes the submissions associated with the group."""
-        submission = Submission.objects.create(user=self.user, part_of_group=self.group)
-        response = self.client.get(self.group_detail_url)
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("submissions", response.context)
-        self.assertIn(submission, response.context["submissions"])
-
-    def test_delete_submission_group(self) -> None:
-        """Test that the submission group can be deleted."""
-        response = self.client.delete(self.group_detail_url)
-        messages = list(get_messages(response.wsgi_request))
-        self.assertEqual(str(messages[0]), gettext("Group deleted"))
-        self.assertRedirects(response, reverse("recordtransfer:user_profile"))
-        self.assertFalse(SubmissionGroup.objects.filter(uuid=self.group.uuid).exists())
-
-    def test_delete_other_user_submission_group(self) -> None:
-        """Test that a user cannot delete another user's submission group."""
-        other_user = User.objects.create_user(username="otheruser", password="password")
-        other_group = SubmissionGroup.objects.create(
-            name="Other Group", description="Other Description", created_by=other_user
-        )
-        url = reverse("recordtransfer:submission_group_detail", kwargs={"uuid": other_group.uuid})
-        response = self.client.delete(url)
-        self.assertEqual(response.status_code, 404)
-
-    @patch("recordtransfer.views.post_submission.SubmissionGroup.delete")
-    def test_delete_submission_group_error(self, mock_delete: MagicMock) -> None:
-        """Test that an error during deletion is handled gracefully."""
-        mock_delete.side_effect = Exception("Deletion error")
-        response = self.client.delete(self.group_detail_url)
-        messages = list(get_messages(response.wsgi_request))
-        self.assertEqual(str(messages[0]), gettext("There was an error deleting the group"))
-        self.assertRedirects(response, reverse("recordtransfer:user_profile"))
 
 
 class TestSubmissionDetailView(TestCase):
