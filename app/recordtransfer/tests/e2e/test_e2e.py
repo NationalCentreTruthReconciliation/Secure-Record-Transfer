1745a6f0aebfbc44de26e08f7a61302c985cdfb9
Eashana <eashanasinghal@gmail.com>
2025-05-28 12:04:57 -0500

    Revert changes made to test_e2e.py file

--- app/recordtransfer/tests/e2e/test_e2e.py
+++ app/recordtransfer/tests/e2e/test_e2e.py
@@ -411,12 +411,9 @@ class SubmissionFormWizardTest(StaticLiveServerTestCase):
         # Click the button to show the add new group dialog
         driver.find_element(By.ID, "show-add-new-group-dialog").click()
 
-        WebDriverWait(driver, 10).until(
-            EC.visibility_of_element_located((By.ID, "id_create_group_button"))
-        )
-        WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.ID, "id_create_group_button"))
-        )
+        # Wait for the modal to appear
+        WebDriverWait(driver, 2)
+
         # Fill out the form in the modal
         group_name_input = driver.find_element(By.ID, "id_submission_group_name")
         group_description_input = driver.find_element(By.ID, "id_submission_group_description")
@@ -424,14 +421,11 @@ class SubmissionFormWizardTest(StaticLiveServerTestCase):
         group_name_input.send_keys(data["name"])
         group_description_input.send_keys(data["description"])
 
-        create_group_button = driver.find_element(By.ID, "id_create_group_button")
-        driver.execute_script("arguments[0].scrollIntoView(true);", create_group_button)
-
         # Submit the form to create the new group
-        create_group_button.click()
+        driver.find_element(By.ID, "id_create_group_button").click()
 
         # Wait for the modal to close and the new group to be added to the select options
-        WebDriverWait(driver, 5).until(
+        WebDriverWait(driver, 2).until(
             EC.invisibility_of_element_located((By.ID, "id_create_group_button"))
         )
 
@@ -763,8 +757,12 @@ class SubmissionFormWizardTest(StaticLiveServerTestCase):
         in_progress_tab.click()
 
         # Look for resume icon and click it
-        resume_link = driver.find_element(By.XPATH, "//a[@data-tip='Resume']")
+        resume_link = driver.find_element(
+            By.XPATH,
+            "//a[@data-tip='Resume']"
+        )
         resume_link.click()
+
 
         # Wait for the Contact Information step to load
         WebDriverWait(driver, 10).until(
