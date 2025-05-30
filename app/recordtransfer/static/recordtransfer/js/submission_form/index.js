88b6a70bfca9f07a52ece7f3572dd402bc9e2b0a
Eashana <eashanasinghal@gmail.com>
2025-05-30 11:42:10 -0500

    Remove unecessary css imports in submission_form/index.js

--- app/recordtransfer/static/recordtransfer/js/submission_form/index.js
+++ app/recordtransfer/static/recordtransfer/js/submission_form/index.js
@@ -1,6 +1,4 @@
 /* global singleCaptchaFn */
-import "../../css/submission_form/review_step.css";
-import "../../css/submission_form/uppy.css";
 import { setupHelpTooltips } from "../base/tooltip";
 import { setupContactInfoForm } from "./contactInfo";
 import { setupOtherIdentifiersForm } from "./otherIdentifiers";
