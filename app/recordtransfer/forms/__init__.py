e6a5f6085f5f6f952f5ed4c99dfa07c14aab5fc2
Kelvin Oo <kelvinoo123@gmail.com>
2025-06-02 14:49:09 -0500

    Fix Ruff lint errors for forms/__init__.py

--- app/recordtransfer/forms/__init__.py
+++ app/recordtransfer/forms/__init__.py
@@ -1,3 +1,5 @@
-from recordtransfer.forms.admin_forms import *
-from recordtransfer.forms.submission_forms import *
-from recordtransfer.forms.user_forms import *
+"""Provides all forms used in the Record Transfer application."""
+
+from recordtransfer.forms.admin_forms import *  # noqa: F403
+from recordtransfer.forms.submission_forms import *  # noqa: F403
+from recordtransfer.forms.user_forms import *  # noqa: F403
