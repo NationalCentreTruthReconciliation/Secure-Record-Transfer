e54720c40324132c5114c9fcb487d30da029aaad
Kelvin Oo <kelvinoo123@gmail.com>
2025-06-02 10:34:15 -0500

    Use is_active field for generating hash value

--- app/recordtransfer/tokens.py
+++ app/recordtransfer/tokens.py
@@ -2,14 +2,21 @@ from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
 from recordtransfer.models import User
 
+
 class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
+    """Strategy object used to generate and check tokens for user account activation."""
+
     def __init__(self):
         super().__init__()
         # sha1 is 40 characters. Since _make_token_with_timestamp returns every second character of
         # the hash hexdigest, we know the token will be 20 characters long.
-        self.algorithm = 'sha1'
+        self.algorithm = "sha1"
 
-    def _make_hash_value(self, user: User, timestamp):
-        return str(user.pk) + str(timestamp) + str(user.confirmed_email)
+    def _make_hash_value(self, user: User, timestamp: int): # type: ignore
+        """Create a hash input value to be used in the token generation. The generated token
+        is invalidated if the user is active or if the timestamp is too old.
+        """
+        return f"{user.pk}{timestamp}{user.is_active}"
+
 
 account_activation_token = AccountActivationTokenGenerator()
