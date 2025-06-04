f6593e415500e38e118ba311e2d10411df5dc444
Kelvin Oo <kelvinoo123@gmail.com>
2025-06-04 06:53:03 -0700

    Apply suggestion to avoid wildcard import in __init__.py
    Co-authored-by: Daniel Lovegrove <d.lovegrove11@gmail.com>

--- app/recordtransfer/forms/__init__.py
+++ app/recordtransfer/forms/__init__.py
@@ -1,5 +1,46 @@
 """Provides all forms used in the Record Transfer application."""
 
-from recordtransfer.forms.admin_forms import *  # noqa: F403
-from recordtransfer.forms.submission_forms import *  # noqa: F403
-from recordtransfer.forms.user_forms import *  # noqa: F403
+from recordtransfer.forms.admin_forms import SubmissionModelForm
+from recordtransfer.forms.submission_forms import (
+    AcceptLegal,
+    ContactInfoForm,
+    ExtendedRecordDescriptionForm,
+    FinalStepFormNoUpload,
+    GroupSubmissionForm,
+    OtherIdentifiersForm,
+    OtherIdentifiersFormSet,
+    RecordDescriptionForm,
+    ReviewForm,
+    ReviewFormItem,
+    RightsForm,
+    RightsFormSet,
+    SourceInfoForm,
+    SubmissionForm,
+    UploadFilesForm,
+    clear_form_errors,
+)
+from recordtransfer.forms.submission_group_form import SubmissionGroupForm
+from recordtransfer.forms.user_forms import SignUpForm, UserProfileForm
+
+__all__ = [
+    "AcceptLegal",
+    "ContactInfoForm",
+    "ExtendedRecordDescriptionForm",
+    "FinalStepFormNoUpload",
+    "GroupSubmissionForm",
+    "OtherIdentifiersForm",
+    "OtherIdentifiersFormSet",
+    "RecordDescriptionForm",
+    "ReviewForm",
+    "ReviewFormItem",
+    "RightsForm",
+    "RightsFormSet",
+    "SignUpForm",
+    "SourceInfoForm",
+    "SubmissionForm",
+    "SubmissionGroupForm",
+    "SubmissionModelForm",
+    "UploadFilesForm",
+    "UserProfileForm",
+    "clear_form_errors",
+]
