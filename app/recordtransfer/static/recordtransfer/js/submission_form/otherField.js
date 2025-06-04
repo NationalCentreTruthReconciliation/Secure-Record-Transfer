28e92ee61d8c5b53e164a78bdbb9394ee2edf2e7
Kelvin Oo <kelvinoo123@gmail.com>
2025-06-04 17:08:31 -0500

    Compare other value as String instead of Number

--- app/recordtransfer/static/recordtransfer/js/submission_form/otherField.js
+++ app/recordtransfer/static/recordtransfer/js/submission_form/otherField.js
@@ -3,7 +3,8 @@
  * .hidden-item class to the other field's parent to show/hide the element.
  * @param {string} selectFieldInputId The ID of the select element
  * @param {string} otherFieldInputId The ID of the input to conditionally show
- * @param {string} otherValue The value of the select element needed to show the other element
+ * @param {string|number} otherValue The value of the select element needed to show the other
+ * element
  */
 export function setupSelectOtherToggle(selectFieldInputId, otherFieldInputId, otherValue) {
     const selectField = document.getElementById(selectFieldInputId);
@@ -21,13 +22,13 @@ export function setupSelectOtherToggle(selectFieldInputId, otherFieldInputId, ot
  * Helper function to show/hide an "other" field based on the value of a select field.
  * @param {HTMLSelectElement} selectField - The select element that triggers the visibility change
  * @param {HTMLInputElement} otherInput - The "Other" input field element to show/hide
- * @param {number} otherValue - The select option value that should trigger showing the "Other"
- * field
+ * @param {string|number} otherValue - The select option value that should trigger showing the
+ * "Other" field
  */
 const updateOtherFieldVisibility = (selectField, otherInput, otherValue) => {
     if (!otherInput) {
         return;
     }
-    const shouldShow = Number(selectField.value) === Number(otherValue);
+    const shouldShow = selectField.value === String(otherValue);
     otherInput.parentElement.classList.toggle("hidden-item", !shouldShow);
 };
