f03c71a64005a7b90eaf5172cfeab11fec15b0e9
Eashana <eashanasinghal@gmail.com>
2025-06-11 08:28:28 -0500

    Change storage for development mode where hashing would be used

--- app/app/settings/docker_dev.py
+++ app/app/settings/docker_dev.py
@@ -96,3 +96,10 @@ LOGGING = {
         },
     },
 }
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {"BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"},
+}
