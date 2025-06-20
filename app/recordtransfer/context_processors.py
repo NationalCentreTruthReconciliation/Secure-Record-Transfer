05771b9a5ab42ce5045c1e6a48db4a7745436ee8
Eashana <eashanasinghal@gmail.com>
2025-06-20 10:46:58 -0500

    Make return type more generic to support passing of Accepted_file_types

--- app/recordtransfer/context_processors.py
+++ app/recordtransfer/context_processors.py
@@ -16,7 +16,7 @@ def file_upload_status(request: HttpRequest) -> dict[str, Any]:
     return {"FILE_UPLOAD_ENABLED": settings.FILE_UPLOAD_ENABLED}
 
 
-def constants_context(request: HttpRequest) -> dict[str, str]:
+def constants_context(request: HttpRequest) -> dict[str, Any]:
     """Make constants available globally in all templates."""
     return {
         **constants.HtmlIds().asdict(),
