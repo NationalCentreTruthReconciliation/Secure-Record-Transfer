7b13bc5cd5a044a67f46c53be4b2d11043b522be
Eashana <eashanasinghal@gmail.com>
2025-06-23 08:40:10 -0500

    Undo changes to context_processors.py

--- app/recordtransfer/context_processors.py
+++ app/recordtransfer/context_processors.py
@@ -11,7 +11,7 @@ def signup_status(request: HttpRequest) -> dict[str, Any]:
     return {"SIGN_UP_ENABLED": settings.SIGN_UP_ENABLED}
 
 
-def file_upload_status(request: HttpRequest) -> dict[str, Any]:
+def file_upload_status(request: HttpRequest) -> dict[str, str]:
     """Add file upload status to template context."""
     return {"FILE_UPLOAD_ENABLED": settings.FILE_UPLOAD_ENABLED}
 
