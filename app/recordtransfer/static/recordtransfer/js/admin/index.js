3cbfba98b2da2e94e1946aeb8387e6cebcad3e69
Kelvin Oo <kelvinoo123@gmail.com>
2025-06-24 17:25:07 -0500

    Remove console log for admin page initialization

--- app/recordtransfer/static/recordtransfer/js/admin/index.js
+++ app/recordtransfer/static/recordtransfer/js/admin/index.js
@@ -1,8 +1,6 @@
 import { setupSelectOtherToggle } from "../utils/otherField.js";
 import { setupPhoneNumberMask } from "../utils/phoneNumberMask.js";
 
-console.log("Initializing admin page setup...");
-
 document.addEventListener("DOMContentLoaded", () => {
     let context = null;
     const contextElement = document.getElementById("py_context_admin");
