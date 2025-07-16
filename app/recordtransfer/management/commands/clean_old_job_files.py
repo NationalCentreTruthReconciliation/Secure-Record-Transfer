9d4255885709d05693f557f4ff998b812cdf0f89
Eashana <eashanasinghal@gmail.com>
2025-07-16 10:03:20 -0500

    Add docstring for the function

--- app/recordtransfer/management/commands/clean_old_job_files.py
+++ app/recordtransfer/management/commands/clean_old_job_files.py
@@ -11,6 +11,13 @@ class Command(BaseCommand):
     """Remove job attachment files older than a specified number of days (based on job.end_time)."""
 
     def add_arguments(self, parser: "argparse.ArgumentParser") -> None:
+        """Add command-line arguments for the management command.
+
+        Parameters
+        ----------
+        parser : argparse.ArgumentParser
+            The parser to which arguments should be added.
+        """
         parser.add_argument(
             "--older-than-days",
             type=int,
