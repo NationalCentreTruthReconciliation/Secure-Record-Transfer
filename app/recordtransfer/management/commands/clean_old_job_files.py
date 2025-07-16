97b1b1e47959c8900c41ae4029267f07d7282aca
Eashana <eashanasinghal@gmail.com>
2025-07-16 10:18:24 -0500

    Calculate cutoff time in handle func

--- app/recordtransfer/management/commands/clean_old_job_files.py
+++ app/recordtransfer/management/commands/clean_old_job_files.py
@@ -1,6 +1,8 @@
 import argparse
+from datetime import timedelta
 
 from django.core.management.base import BaseCommand
+from django.utils import timezone
 
 
 class Command(BaseCommand):
@@ -22,3 +24,7 @@ class Command(BaseCommand):
             required=True,
             help="Delete job attachment files for jobs whose end_time is older than this many days.",
         )
+
+    def handle(self, *args, **options) -> None:
+        days = options["older_than_days"]
+        cutoff = timezone.now() - timedelta(days=days)
