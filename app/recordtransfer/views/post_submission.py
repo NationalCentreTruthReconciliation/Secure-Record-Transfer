0ec0ef6ce414ffcd2c5671c99e701da994be6425
Kelvin Oo <kelvinoo123@gmail.com>
2025-07-25 10:15:08 -0500

    Include header in request to fetch submission groups

--- app/recordtransfer/views/post_submission.py
+++ app/recordtransfer/views/post_submission.py
@@ -4,6 +4,7 @@ import logging
 from typing import Any, Optional, cast
 
 from caais.export import ExportVersion
+from django.core.exceptions import BadRequest
 from django.db.models import QuerySet
 from django.forms import BaseModelForm
 from django.http import (
@@ -21,7 +22,7 @@ from django.views.decorators.http import require_http_methods
 from django.views.generic import DetailView, UpdateView
 from django_htmx.http import trigger_client_event
 
-from recordtransfer.constants import HtmlIds, QueryParameters
+from recordtransfer.constants import HeaderNames, HtmlIds, QueryParameters
 from recordtransfer.forms.submission_group_form import SubmissionGroupForm
 from recordtransfer.models import Submission, SubmissionGroup, User
 
@@ -207,6 +208,9 @@ class SubmissionGroupDetailView(UpdateView):
 @require_http_methods(["GET"])
 def get_user_submission_groups(request: HttpRequest) -> JsonResponse:
     """Return a JSON response containing all submission groups created by the specified user."""
+    if not request.headers.get(HeaderNames.FRONTEND_REQUEST):
+        raise BadRequest("Direct access is not allowed.")
+
     user: User = cast(User, request.user)
     submission_groups = SubmissionGroup.objects.filter(created_by=user)
     groups = [
