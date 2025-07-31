d3ab8b7d3e6cb04d6de7cebd1413a5a1ea43a405
Eashana <eashanasinghal@gmail.com>
2025-07-31 10:23:22 -0500

    Revert "Fix unmigrated changes error"
    This reverts commit 549673b759dd28ed2e00fb408deb702844c46171.

--- app/recordtransfer/utils.py
+++ app/recordtransfer/utils.py
@@ -362,11 +362,12 @@ def get_js_translation_version() -> str:
 
     This changes whenever compiled JS translations are updated.
     """
-    mtimes = [
-        item.stat().st_mtime
-        for locale_dir in settings.LOCALE_PATHS
-        for item in Path(locale_dir).rglob("djangojs.mo")
-    ]
-    if not mtimes:
-        return "0"
-    return str(max(mtimes))
+    return str(
+        max(
+            [
+                item.stat().st_mtime
+                for locale_dir in settings.LOCALE_PATHS
+                for item in Path(locale_dir).rglob("djangojs.mo")
+            ]
+        )
+    )
