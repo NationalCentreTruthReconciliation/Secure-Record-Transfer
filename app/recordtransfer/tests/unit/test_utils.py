4041f5ceb528f1f46a0b61630928a7a3e375993c
Daniel Lovegrove <d.lovegrove11@gmail.com>
2025-07-31 11:59:54 -0500

    Fix typo
    Co-authored-by: Eashana Singhal <106555593+Eashana0104@users.noreply.github.com>

--- app/recordtransfer/tests/unit/test_utils.py
+++ app/recordtransfer/tests/unit/test_utils.py
@@ -417,6 +417,6 @@ class TestGetJsTranslationVersion(TestCase):
 
     @override_settings(LOCALE_PATHS=[])
     def test_no_locale_paths(self) -> None:
-        """Test that a default value of '0' is returned if there are not paths."""
+        """Test that a default value of '0' is returned if there are no paths."""
         version = get_js_translation_version()
         self.assertEqual("0", version)
