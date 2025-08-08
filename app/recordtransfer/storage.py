40900f8b8772b9c4b69b5ea9209d290a36cebdf3
Eashana Singhal <106555593+Eashana0104@users.noreply.github.com>
2025-08-08 11:53:47 -0500

    Fix ruff linting error in storage.py

--- app/recordtransfer/storage.py
+++ app/recordtransfer/storage.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from django.conf import settings
+from django.core.files.base import File
 from django.core.files.storage import FileSystemStorage
 
 
@@ -12,11 +13,14 @@ class OverwriteStorage(FileSystemStorage):
     overwrites any file being saved with a path that already exists.
     """
 
-    def _save(self, name, content):
+    def _save(self, name: str, content: File) -> str:
         self.delete(name)
         return super()._save(name, content)
 
-    def get_available_name(self, name, max_length=None):
+    def get_available_name(self, name: str, max_length: int | None = None) -> str:
+        """Return the provided file name as the available name,
+        allowing overwriting of existing files.
+        """
         return name
 
 
