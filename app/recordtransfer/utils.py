3865809cb97e6352174d5295fdfe7d24a503229e
Eashana Singhal <106555593+Eashana0104@users.noreply.github.com>
2025-08-18 13:42:27 -0500

    Update app/recordtransfer/utils.py
    Co-authored-by: Daniel Lovegrove <d.lovegrove11@gmail.com>

--- app/recordtransfer/utils.py
+++ app/recordtransfer/utils.py
@@ -547,7 +547,7 @@ def is_deployed_environment() -> bool:
 
 def get_client_ip_address(request: HttpRequest) -> str:
     """Get the client's IP address from the request."""
-    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+    x_forwarded_for = request.headers.get("x-forwarded-for")
     if x_forwarded_for:
         ip = x_forwarded_for.split(",")[0]
     else:
