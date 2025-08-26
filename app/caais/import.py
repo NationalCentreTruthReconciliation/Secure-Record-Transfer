66bb8034b56138953b446f6590c5b5a15837733f
Daniel Lovegrove <d.lovegrove11@gmail.com>
2025-08-26 15:04:48 -0500

    Add a helper method for splitting multi-value cells

--- app/caais/import.py
+++ app/caais/import.py
@@ -1,4 +1,5 @@
 from caais.csvfile import Columns
+from caais.db import MULTI_VALUE_SEPARATOR
 
 
 def validate_fieldnames(row: dict, columns: Columns = Columns.CAAIS_1_0) -> None:
@@ -27,3 +28,39 @@ def validate_fieldnames(row: dict, columns: Columns = Columns.CAAIS_1_0) -> None
                 cols=", ".join(sorted(missing_cols, key=lambda c: columns.fieldnames.index(c)))
             )
         )
+
+
+def split_cell(row: dict, *cols) -> tuple[list[str]]:
+    """Split cells from one or more columns by the MULTI_VALUE_SEPARATOR.
+
+    If multiple cols are passed, the number of elements in each col after splitting is compared. If
+    there is a mismatch in the number of elements between any column, a ValueError is raised.
+    """
+    el_count = -1
+
+    split = []
+
+    for i, col in enumerate(cols):
+        col_split = list(map(str.strip, row[col].split(MULTI_VALUE_SEPARATOR)))
+        curr_el_count = len(col_split)
+
+        # Compare all future counts against this first count
+        if el_count == -1:
+            el_count = curr_el_count
+
+        elif el_count != curr_el_count:
+            # We've only enumerated up to the current column, so we can only use previous columns
+            others = ", ".join(f"'{c}'" for c in cols[0:i])
+            raise ValueError(
+                f"The column '{col}' has a different number of elements compared to these other "
+                f"columns: {others}. These columns are all required to have the same number of "
+                f"elements, separated by a {MULTI_VALUE_SEPARATOR} character"
+            )
+
+        clean_col_split = [
+            value if value and value.lower() != "null" else None for value in col_split
+        ]
+
+        split.append(clean_col_split)
+
+    return tuple(split)
