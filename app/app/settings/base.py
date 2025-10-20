fb8e586b183fdab695e887ac8b5a6e6bb8438337
Daniel Lovegrove <d.lovegrove11@gmail.com>
2025-10-20 14:00:19 -0500

    Remove unneeded change to settings

--- app/app/settings/base.py
+++ app/app/settings/base.py
@@ -77,7 +77,6 @@ ROOT_URLCONF = "app.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
