267a9c1d1dd4c40f68c179f8a9b5057c67a94036
Daniel Lovegrove <d.lovegrove11@gmail.com>
2025-12-12 14:44:24 -0600

    Set up ESLint for Typescript

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
-import pluginJs from "@eslint/js";
-import stylisticJs from "@stylistic/eslint-plugin-js";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import stylisticJs from "@stylistic/eslint-plugin";
 import importPlugin from "eslint-plugin-import";
 import jsdoc from "eslint-plugin-jsdoc";
 import globals from "globals";
@@ -12,11 +13,11 @@ export default [
             "{env,venv,.env,.venv}/**",
             "dist/",
             "node_modules/",
-            "app/static/",
             "docs/"
         ],
     },
     {
+        files: ["app/**/*.{js,ts}"],
         languageOptions: { globals: globals.browser },
         plugins: {
             "@stylistic/js": stylisticJs,
@@ -41,13 +42,33 @@ export default [
         }
     },
     {
-        files: ["webpack.config.js"],
-        languageOptions: {
-            globals: globals.node,
+        files: ["build.ts", "scripts/**/*.ts"],
+        languageOptions: { globals: { ...globals.nodeBuiltin, Bun: "readonly" } },
+        plugins: {
+            "@stylistic/js": stylisticJs,
+            "import": importPlugin,
+            "jsdoc": jsdoc,
         },
+        rules: {
+            // Plugin rules
+            "@stylistic/js/indent": ["error", 4],
+            "@stylistic/js/max-len": ["error", { code: 99, tabWidth: 4, ignoreUrls: true }],
+            "@stylistic/js/semi": ["error", "always"],
+            "@stylistic/js/quotes": ["error", "double"],
+            "import/order": ["error", {
+                "alphabetize": { "order": "asc", "caseInsensitive": true }
+            }],
+            // ESLint extra rules
+            "no-console": ["warn", { "allow": ["error", "warn", "info"] }],
+            "eqeqeq": "error",
+            "curly": "error",
+            "prefer-const": "error",
+            "no-duplicate-imports": "error",
+        }
     },
     // Docstring rules
     jsdoc.configs["flat/recommended"],
     // Recommended ESLint rules
-    pluginJs.configs.recommended,
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
 ];
