4820436d3c644b62dff82e3d51cd7b0c70264bae
Eashana Singhal <eashanasinghal@gmail.com>
2025-12-22 10:48:12 -0600

    Fix failing unit tests

--- app/recordtransfer/tests/unit/views/test_media.py
+++ app/recordtransfer/tests/unit/views/test_media.py
@@ -131,7 +131,10 @@ class TestJobFileView(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response["X-Accel-Redirect"].endswith("bag.zip"))
-        self.assertEqual(response["Content-Type"], "application/zip")
+        self.assertIn(
+            response["Content-Type"],
+            ["application/zip", "application/x-zip-compressed"],
+        )
         self.assertEqual(response["Content-Disposition"], 'attachment; filename="my_bag.zip"')
 
     @override_settings(DEBUG=False)
